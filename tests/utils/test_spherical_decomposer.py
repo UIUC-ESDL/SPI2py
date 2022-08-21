728838ff7cc557ce3238f48313b7574276441f01
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2022-08-21 18:51:53 -0400

    Implement JIT

--- tests/utils/test_spherical_decomposer.py
+++ tests/utils/test_spherical_decomposer.py
@@ -5,5 +5,5 @@ def test_no_origin():
     pos, rad = generate_rectangular_prism([1, 1, 1], 0.5)
 
 
-def test_origin()
+def test_origin():
     pos, rad = generate_rectangular_prism([1, 1, 1], 0.5, [1, 1, 1])
