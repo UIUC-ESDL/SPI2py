4529f96e87d0e3d84defdef62422eed9186b518a
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2022-08-31 10:36:56 -0500

    Partially implement JAX

--- tests/test_utils/test_distance_calculations/test_min_line_line_distance.py
+++ tests/test_utils/test_distance_calculations/test_min_line_line_distance.py
@@ -1,4 +1,4 @@
-import numpy as np
+import jax.numpy as jnp
 from utils.distance_calculations import min_line_line_distance
 
 # Add test to this and other dist for list vs np array
@@ -6,28 +6,28 @@ from utils.distance_calculations import min_line_line_distance
 
 def test_parallel():
 
-    a0 = np.array([0., 0., 0.])
-    a1 = np.array([1., 0., 0.])
-    b0 = np.array([0., 0., 1.])
-    b1 = np.array([1., 0., 1.])
+    a0 = jnp.array([0., 0., 0.])
+    a1 = jnp.array([1., 0., 0.])
+    b0 = jnp.array([0., 0., 1.])
+    b1 = jnp.array([1., 0., 1.])
 
     dist = min_line_line_distance(a0, a1, b0, b1)
 
     print('dist', dist)
 
-    assert dist == 1
+    assert round(dist,2) == 1
 
 
 def test_skew():
 
-    a0 = np.array([0., 0., 0.])
-    a1 = np.array([1., 0., 0.])
-    b0 = np.array([0., 0., 2.])
-    b1 = np.array([1., 0., 1.])
+    a0 = jnp.array([0., 0., 0.])
+    a1 = jnp.array([1., 0., 0.])
+    b0 = jnp.array([0., 0., 2.])
+    b1 = jnp.array([1., 0., 1.])
 
     dist = min_line_line_distance(a0, a1, b0, b1)
 
     print('dist', dist)
 
-    assert dist == 1
+    assert round(dist,2) == 1
 
--- utils/distance_calculations.py
+++ utils/distance_calculations.py
@@ -2,13 +2,19 @@
 ...
 """
 
-import numpy as np
+# import numpy as np
 from scipy.spatial.distance import cdist, pdist
-from numba import njit
+
+import jax.numpy as jnp
+from jax import grad, jit, vmap
 
 
 def min_cdist(a, b):
-    return np.min(cdist(a, b))
+    return jnp.min(cdist(a, b))
+
+
+def min_cdist_jit(a, b):
+    return jnp.min(cdist(a, b))
 
 
 def min_kdtree_distance(tree, positions):
@@ -21,26 +27,26 @@ def min_kdtree_distance(tree, positions):
     """
 
     dist, _ = tree.query(positions)
-    min_dist = np.min(dist)
+    min_dist = jnp.min(dist)
 
     return min_dist
 
 
-@njit
+@jit
 def min_point_line_distance(p, a, b):
     """
     Function not tested for all conditions yet
 
-    :param p:
-    :param a:
-    :param b:
+    :param p: (3,)
+    :param a: (3,)
+    :param b: (3,)
     :return:
     """
 
-    return np.linalg.norm(np.dot(p - b, a - b) / np.dot(a - b, a - b) * (a - b) + b - p)
+    return jnp.linalg.norm(jnp.dot(p - b, a - b) / jnp.dot(a - b, a - b) * (a - b) + b - p)
 
 
-# @njit
+# @jit
 def min_line_line_distance(a0, a1, b0, b1):
     """
     Function not tested for all conditions yet
@@ -62,47 +68,47 @@ def min_line_line_distance(a0, a1, b0, b1):
     # Calculate denomitator
     A = a1 - a0
     B = b1 - b0
-    magA = np.linalg.norm(A)
-    magB = np.linalg.norm(B)
+    magA = jnp.linalg.norm(A)
+    magB = jnp.linalg.norm(B)
 
     _A = A / magA
     _B = B / magB
 
-    cross = np.cross(_A, _B);
-    denom = np.linalg.norm(cross) ** 2
+    cross = jnp.cross(_A, _B);
+    denom = jnp.linalg.norm(cross) ** 2
 
     # If lines are parallel (denom=0) test if lines overlap.
     # If they don't overlap then there is a closest point solution.
     # If they do overlap, there are infinite closest positions, but there is a closest distance
     if not denom:
-        d0 = np.dot(_A, (b0 - a0))
+        d0 = jnp.dot(_A, (b0 - a0))
 
         # Overlap only possible with clamping
         if clampA0 or clampA1 or clampB0 or clampB1:
-            d1 = np.dot(_A, (b1 - a0))
+            d1 = jnp.dot(_A, (b1 - a0))
 
             # Is segment B before A?
             if d0 <= 0 >= d1:
                 if clampA0 and clampB1:
-                    if np.absolute(d0) < np.absolute(d1):
-                        return a0, b0, np.linalg.norm(a0 - b0)
-                    return a0, b1, np.linalg.norm(a0 - b1)
+                    if jnp.absolute(d0) < jnp.absolute(d1):
+                        return a0, b0, jnp.linalg.norm(a0 - b0)
+                    return a0, b1, jnp.linalg.norm(a0 - b1)
 
 
             # Is segment B after A?
             elif d0 >= magA <= d1:
                 if clampA1 and clampB0:
-                    if np.absolute(d0) < np.absolute(d1):
-                        return a1, b0, np.linalg.norm(a1 - b0)
-                    return a1, b1, np.linalg.norm(a1 - b1)
+                    if jnp.absolute(d0) < jnp.absolute(d1):
+                        return a1, b0, jnp.linalg.norm(a1 - b0)
+                    return a1, b1, jnp.linalg.norm(a1 - b1)
 
         # Segments overlap, return distance between parallel segments
-        return np.linalg.norm(((d0 * _A) + a0) - b0)
+        return jnp.linalg.norm(((d0 * _A) + a0) - b0)
 
     # Lines criss-cross: Calculate the projected closest points
     t = (b0 - a0);
-    detA = np.linalg.det([t, _B, cross])
-    detB = np.linalg.det([t, _A, cross])
+    detA = jnp.linalg.det([t, _B, cross])
+    detB = jnp.linalg.det([t, _A, cross])
 
     t0 = detA / denom;
     t1 = detB / denom;
@@ -124,7 +130,7 @@ def min_line_line_distance(a0, a1, b0, b1):
 
         # Clamp projection A
         if (clampA0 and t0 < 0) or (clampA1 and t0 > magA):
-            dot = np.dot(_B, (pA - b0))
+            dot = jnp.dot(_B, (pA - b0))
             if clampB0 and dot < 0:
                 dot = 0
             elif clampB1 and dot > magB:
@@ -133,14 +139,14 @@ def min_line_line_distance(a0, a1, b0, b1):
 
         # Clamp projection B
         if (clampB0 and t1 < 0) or (clampB1 and t1 > magB):
-            dot = np.dot(_A, (pB - a0))
+            dot = jnp.dot(_A, (pB - a0))
             if clampA0 and dot < 0:
                 dot = 0
             elif clampA1 and dot > magA:
                 dot = magA
             pA = a0 + (_A * dot)
 
-    return pA, pB, np.linalg.norm(pA - pB)
+    return pA, pB, jnp.linalg.norm(pA - pB)
 
 
 def calculate_gap(radius1, radius2, min_dist):
