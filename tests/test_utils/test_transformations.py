53f572ffb93d7e2f3d94cbc6c1d80bc67931dcb1
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2022-10-07 14:54:21 -0500

    Commit before branch

--- tests/test_utils/test_transformations.py
+++ tests/test_utils/test_transformations.py
@@ -6,36 +6,33 @@ Use round to avoid small numerical differences
 """
 
 import numpy as np
-from utils.transformations import rotate
+from src.SPI2Py.utils.transformations import rotate
 
+positions = np.array([[2., 2., 0.], [4., 2., 0.], [4, 0, 0]])
+current_angle = np.array([0., 0., 0.])
 
-positions = np.array([[2.,2.,0.],[4.,2.,0.],[4,0,0]])
-current_angle = np.array([0.,0.,0.])
 
 def test_rotate_x_cc():
-
     new_angle_x_90 = np.array([np.pi / 2, 0., 0.])
     delta_rotation = new_angle_x_90 - current_angle
-    new_positions = np.round(rotate(positions,delta_rotation),0)
+    new_positions = np.round(rotate(positions, delta_rotation), 0)
 
-    expected = np.array([[2.,2.,0.],[4.,2.,0.],[4.,2.,-2.]])
+    expected = np.array([[2., 2., 0.], [4., 2., 0.], [4., 2., -2.]])
 
     assert np.array_equal(new_positions, expected)
 
 
 def test_rotate_y_cc():
-
     new_angle_y_90 = np.array([0., np.pi / 2, 0.])
     delta_rotation = new_angle_y_90 - current_angle
-    new_positions = np.round(rotate(positions,delta_rotation),0)
+    new_positions = np.round(rotate(positions, delta_rotation), 0)
 
-    expected = np.array([[2.,2.,0.],[2.,2.,-2.],[2.,0.,-2.]])
+    expected = np.array([[2., 2., 0.], [2., 2., -2.], [2., 0., -2.]])
 
     assert np.array_equal(new_positions, expected)
 
 
 def test_rotate_z_cc():
-
     new_angle_z_90 = np.array([0., 0., np.pi / 2])
     delta_rotation = new_angle_z_90 - current_angle
     new_positions = np.round(rotate(positions, delta_rotation), 0)
