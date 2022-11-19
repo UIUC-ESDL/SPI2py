2c69118ea2e0e59392aca78376c8d1944b3afa1a
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2022-11-19 13:50:15 -0600

    Fix rotate function and accompanying unit tests + reconfig pyprojct for unit tests

--- tests/test_utils/test_spatial_transformations.py
+++ tests/test_utils/test_spatial_transformations.py
@@ -3,30 +3,29 @@
 Here is a helpful checker for test cases: https://www.emathhelp.net/calculators/algebra-2/rotation-calculator/
 
 Use round to avoid small numerical differences
+
+TODO write tests to ensure negative values are same e.g., 90 degrees ccw (-90) = 270 degrees cw (270)
 """
 
 import numpy as np
 from src.SPI2Py.utils.spatial_transformations import rotate
-from scipy.spatial.transform import Rotation as R
-
-positions = np.array([[2., 2., 0.], [4., 2., 0.], [4, 0, 0]])
 
 
-def test_dummy():
-    assert 1== 1
+positions = np.array([[2., 2., 0.], [4., 2., 0.], [4., 0., 0.]])
 
-def test_rotate_x_cc():
 
-    rotation_x_90 = np.array([np.pi / 2, 0., 0.])
+def test_rotate_x_90():
 
+    rotation_x_90 = np.array([np.pi / 2., 0., 0.])
+
+    # Round to avoid 0.9999 != 1, but keep at least one decimal point to avoid njit errors
+    # Njit requires floats not integers!
     new_positions = np.round(rotate(positions, rotation_x_90), 0)
 
-    r = R.from_euler('zyx',[0,0,90], degrees=True)
-    new_pos = r.apply(positions)
-
+    # See accompanying PPT file for test case derivations
     expected = np.array([[2., 2., 0.], [4., 2., 0.], [4., 2., -2.]])
 
-    assert np.array_equal(new_pos, expected)
+    assert np.array_equal(new_positions, expected)
 
 
 # def test_rotate_y_cc():
