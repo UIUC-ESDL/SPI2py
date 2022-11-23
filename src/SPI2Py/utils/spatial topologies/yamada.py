7a4601249e950014a3be695a37f7ff3b9c5192d6
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2022-11-22 22:21:39 -0600

    Commenting out enumeration code to debug individual Yamada polynomial calculations

--- src/SPI2Py/utils/spatial topologies/yamada.py
+++ src/SPI2Py/utils/spatial topologies/yamada.py
@@ -205,7 +205,7 @@ def H_poly(abstract_graph):
         return ans
 
 
-class EntryPoint():
+class EntryPoint:
     def __init__(self, vertex_like, index):
         self.vertex = vertex_like
         self.index = index
@@ -229,7 +229,7 @@ class EntryPoint():
         return EntryPoint(W, k)
 
 
-class BaseVertex():
+class BaseVertex:
     """
     A flat vertex has n inputs, labeled 0, 1, ..., n-1 in
     anticlockwise order.
@@ -291,7 +291,7 @@ class Edge(BaseVertex):
         return self.adjacent[(i + 1) % 2]
 
 
-class SpatialGraphDiagram():
+class SpatialGraphDiagram:
     """
     Unknotted theta graph:
 
@@ -579,104 +579,104 @@ class SpatialGraphDiagram():
         return A * Y_plus + (A ** -1) * Y_minus + Y_0
 
 
-def shadows_via_plantri_by_ascii(num_tri_verts, num_crossings):
-    assert num_tri_verts % 2 == 0
-    vertices = num_tri_verts + num_crossings
-    edges = (3 * num_tri_verts + 4 * num_crossings) // 2
-    faces = 2 - vertices + edges
-    cmd = ['./plantri',
-           '-p -d',  # simple planar maps, but return the dual
-           '-f4',  # maximum valence in the returned dual is <= 4
-           '-c1',  # graph should be 1-connected
-           '-m2',  # no valence 1 vertices = no loops in the dual
-           '-a',  # return in ascii format
-           '-e%d' % edges,
-           '%d' % faces]
-    proc = subprocess.run(' '.join(cmd), shell=True, text=True, capture_output=True)
-    ans = []
-    for line in proc.stdout.splitlines():
-        graph_data = line.split()[1].split(',')
-        counts = collections.Counter(len(v) for v in graph_data)
-        assert counts[3] == num_tri_verts and counts[4] == num_crossings
-        ans.append(graph_data)
-    return ans
+# def shadows_via_plantri_by_ascii(num_tri_verts, num_crossings):
+#     assert num_tri_verts % 2 == 0
+#     vertices = num_tri_verts + num_crossings
+#     edges = (3 * num_tri_verts + 4 * num_crossings) // 2
+#     faces = 2 - vertices + edges
+#     cmd = ['./plantri',
+#            '-p -d',  # simple planar maps, but return the dual
+#            '-f4',  # maximum valence in the returned dual is <= 4
+#            '-c1',  # graph should be 1-connected
+#            '-m2',  # no valence 1 vertices = no loops in the dual
+#            '-a',  # return in ascii format
+#            '-e%d' % edges,
+#            '%d' % faces]
+#     proc = subprocess.run(' '.join(cmd), shell=True, text=True, capture_output=True)
+#     ans = []
+#     for line in proc.stdout.splitlines():
+#         graph_data = line.split()[1].split(',')
+#         counts = collections.Counter(len(v) for v in graph_data)
+#         assert counts[3] == num_tri_verts and counts[4] == num_crossings
+#         ans.append(graph_data)
+#     return ans
 
 
-def read_edge_code(stream, size):
-    """
-    Read 1 byte form of edge code
-    """
-    ans = [[]]
-    for _ in range(size):
-        i = int.from_bytes(stream.read(1), 'big')
-        if i < 255:
-            ans[-1].append(i)
-        else:
-            ans.append([])
-    return ans
+# def read_edge_code(stream, size):
+#     """
+#     Read 1 byte form of edge code
+#     """
+#     ans = [[]]
+#     for _ in range(size):
+#         i = int.from_bytes(stream.read(1), 'big')
+#         if i < 255:
+#             ans[-1].append(i)
+#         else:
+#             ans.append([])
+#     return ans
 
 
-def shadows_via_plantri_by_edge_codes(num_tri_verts, num_crossings):
-    assert num_tri_verts % 2 == 0
-    vertices = num_tri_verts + num_crossings
-    edges = (3 * num_tri_verts + 4 * num_crossings) // 2
-    faces = 2 - vertices + edges
-    cmd = ['./plantri',
-           '-p -d',  # simple planar maps, but return the dual
-           '-f4',  # maximum valence in the returned dual is <= 4
-           '-c1',  # graph should be 1-connected
-           '-m2',  # no valence 1 vertices = no loops in the dual
-           '-E',  # return binary edge code format
-           '-e%d' % edges,
-           '%d' % faces]
-    proc = subprocess.run(' '.join(cmd), shell=True, capture_output=True)
-    stdout = io.BytesIO(proc.stdout)
-    assert stdout.read(13) == b'>>edge_code<<'
-    shadows = []
-    while True:
-        b = stdout.read(1)
-        if len(b) == 0:
-            break
-        size = int.from_bytes(b, 'big')
-        assert size != 0
-        shadows.append(read_edge_code(stdout, size))
+# def shadows_via_plantri_by_edge_codes(num_tri_verts, num_crossings):
+#     assert num_tri_verts % 2 == 0
+#     vertices = num_tri_verts + num_crossings
+#     edges = (3 * num_tri_verts + 4 * num_crossings) // 2
+#     faces = 2 - vertices + edges
+#     cmd = ['./plantri',
+#            '-p -d',  # simple planar maps, but return the dual
+#            '-f4',  # maximum valence in the returned dual is <= 4
+#            '-c1',  # graph should be 1-connected
+#            '-m2',  # no valence 1 vertices = no loops in the dual
+#            '-E',  # return binary edge code format
+#            '-e%d' % edges,
+#            '%d' % faces]
+#     proc = subprocess.run(' '.join(cmd), shell=True, capture_output=True)
+#     stdout = io.BytesIO(proc.stdout)
+#     assert stdout.read(13) == b'>>edge_code<<'
+#     shadows = []
+#     while True:
+#         b = stdout.read(1)
+#         if len(b) == 0:
+#             break
+#         size = int.from_bytes(b, 'big')
+#         assert size != 0
+#         shadows.append(read_edge_code(stdout, size))
+#
+#     return shadows
 
-    return shadows
 
-
-class Shadow:
-    def __init__(self, edge_codes):
-        self.edge_codes = edge_codes
-        self.vertices = [edges for edges in edge_codes if len(edges) == 3]
-        self.crossings = [edges for edges in edge_codes if len(edges) == 4]
-        self.num_edges = sum(len(edges) for edges in edge_codes) // 2
-
-    def spatial_graph_diagram(self, signs=None, check=True):
-        num_cross = len(self.crossings)
-        if signs is None:
-            signs = num_cross * [0]
-        else:
-            assert len(signs) == num_cross
-
-        objects = [Edge(i) for i in range(self.num_edges)]
-        for v, edges in enumerate(self.vertices):
-            d = len(edges)
-            V = Vertex(d, 'V%d' % v)
-            objects.append(V)
-            for i, e in enumerate(edges):
-                E = objects[e]
-                e = 0 if E.adjacent[0] is None else 1
-                V[i] = E[e]
-
-        for c, edges in enumerate(self.crossings):
-            C = Crossing('C%d' % c)
-            objects.append(C)
-            for i, e in enumerate(edges):
-                E = objects[e]
-                e = 0 if E.adjacent[0] is None else 1
-                C[(i + signs[c]) % 4] = E[e]
-
-        return SpatialGraphDiagram(objects, check=check)
+# class Shadow:
+#     def __init__(self, edge_codes):
+#         self.edge_codes = edge_codes
+#         self.vertices = [edges for edges in edge_codes if len(edges) == 3]
+#         self.crossings = [edges for edges in edge_codes if len(edges) == 4]
+#         self.num_edges = sum(len(edges) for edges in edge_codes) // 2
+#
+#     def spatial_graph_diagram(self, signs=None, check=True):
+#         num_cross = len(self.crossings)
+#         if signs is None:
+#             signs = num_cross * [0]
+#         else:
+#             assert len(signs) == num_cross
+#
+#         objects = [Edge(i) for i in range(self.num_edges)]
+#         for v, edges in enumerate(self.vertices):
+#             d = len(edges)
+#             V = Vertex(d, 'V%d' % v)
+#             objects.append(V)
+#             for i, e in enumerate(edges):
+#                 E = objects[e]
+#                 e = 0 if E.adjacent[0] is None else 1
+#                 V[i] = E[e]
+#
+#         for c, edges in enumerate(self.crossings):
+#             C = Crossing('C%d' % c)
+#             objects.append(C)
+#             for i, e in enumerate(edges):
+#                 E = objects[e]
+#                 e = 0 if E.adjacent[0] is None else 1
+#                 C[(i + signs[c]) % 4] = E[e]
+#
+#         return SpatialGraphDiagram(objects, check=check)
 
 
 # def spatial_graph_diagrams_from_shadow(shadow, signs):
@@ -714,61 +714,51 @@ class Shadow:
 #                                 yield D
 
 
-def get_coefficients_and_exponents(poly):
-    # Assumes all denominators are only A**n with no coefficient
-    coefficients = poly.numerator().Vec()
-    coeff_len = len(coefficients)
-
-    exponents = []
-    degree = poly.poldegree()
-
-    for _ in range(coeff_len):
-        exponents.append(degree)
-        degree -= 1
-
-    return coefficients, exponents
-
-
-def reverse_poly(poly):
-    """
-    >>> reverse_poly(A**-1 + 2)
-    A + 2
-    """
-    # R = poly.parent()
-    # A = R.gen()
-    A = pari('A')
-
-    # #exps = poly.exponents()
-    # exps = poly.Vec()
-
-    # TODO fix, it's not getting coefficients
-    # # coeffs = poly.coefficients()
-    # coeffs = poly.list()
-
-    coeffs, exps = get_coefficients_and_exponents(poly)
-
-    # #ans = R(0)
-    ans = pari(0)
-
-    for c, e in zip(coeffs, exps):
-        ans += c * A ** (-e)
-
-    return ans
+# def get_coefficients_and_exponents(poly):
+#     # Assumes all denominators are only A**n with no coefficient
+#     coefficients = poly.numerator().Vec()
+#     coeff_len = len(coefficients)
+#
+#     exponents = []
+#     degree = poly.poldegree()
+#
+#     for _ in range(coeff_len):
+#         exponents.append(degree)
+#         degree -= 1
+#
+#     return coefficients, exponents
 
 
-def normalize_poly(poly):
-    """
-    The Yamada polynomial is only defined up to a power of (-A)^n.
+# def reverse_poly(poly):
+#     """
+#     >>> reverse_poly(A**-1 + 2)
+#     A + 2
+#     """
+#
+#     A = pari('A')
+#
+#     coeffs, exps = get_coefficients_and_exponents(poly)
+#
+#     ans = pari(0)
+#
+#     for c, e in zip(coeffs, exps):
+#         ans += c * A ** (-e)
+#
+#     return ans
 
-    Also, we don't want to distinguish between a spatial graph and its
-    mirror image, which corresponds to interchanging A <-> A^(-1).
-    """
-    # A = poly.parent().gen()
-    _, exps = get_coefficients_and_exponents(poly)
-    a, b = min(exps), max(exps)
-    ans1 = (-A) ** (-a) * poly
-    ans2 = (-A) ** b * reverse_poly(poly)
-    return min([ans1, ans2], key=list)
+
+# def normalize_poly(poly):
+#     """
+#     The Yamada polynomial is only defined up to a power of (-A)^n.
+#
+#     Also, we don't want to distinguish between a spatial graph and its
+#     mirror image, which corresponds to interchanging A <-> A^(-1).
+#     """
+#     _, exps = get_coefficients_and_exponents(poly)
+#     a, b = min(exps), max(exps)
+#     ans1 = (-A) ** (-a) * poly
+#     ans2 = (-A) ** b * reverse_poly(poly)
+#     return min([ans1, ans2], key=list)
 
 
 # def enumerate_yamada_classes(G, max_crossings):
@@ -784,10 +774,10 @@ def normalize_poly(poly):
 #     return polys, examined
 
 
-def to_poly(diagram):
-    p = diagram.yamada_polynomial()
-    p = normalize_poly(p)
-    return p, diagram
+# def to_poly(diagram):
+#     p = diagram.yamada_polynomial()
+#     p = normalize_poly(p)
+#     return p, diagram
 
 
 # def enumerate_yamada_classes_multicore(G, max_crossings, pool):
@@ -806,14 +796,14 @@ def to_poly(diagram):
 #     return polys, timings, examined
 
 
-def pickle_yamada(data, filename):
-    with open(filename, 'wb') as file:
-        pickle.dump(data, file)
+# def pickle_yamada(data, filename):
+#     with open(filename, 'wb') as file:
+#         pickle.dump(data, file)
 
 
-def load_yamada(filename):
-    with open(filename, 'rb') as file:
-        return pickle.load(file)
+# def load_yamada(filename):
+#     with open(filename, 'rb') as file:
+#         return pickle.load(file)
 
 
 if __name__ == '__main__':
@@ -824,4 +814,6 @@ if __name__ == '__main__':
     G = nx.MultiGraph([(0, 1), (0, 1), (2, 3), (2, 3), (0, 2), (1, 3)])
     H = nx.complete_graph(6, nx.MultiGraph)
     # H = nx.complete_graph(4, nx.MultiGraph)
+
+    print('main')
 
