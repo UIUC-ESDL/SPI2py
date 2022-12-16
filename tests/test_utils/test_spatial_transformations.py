3e8116fd7d111c38b54a67fc348fea5535b5201d
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2022-12-15 23:53:04 -0600

    Partially implementing InterconnectNode class

--- tests/test_utils/test_spatial_transformations.py
+++ tests/test_utils/test_spatial_transformations.py
@@ -8,7 +8,7 @@ TODO write tests to ensure negative values are same e.g., 90 degrees ccw (-90) =
 """
 
 import numpy as np
-from src.SPI2Py.analysis.spatial_calculations import rotate_about_point
+from src.SPI2Py.analysis.spatial_calculations.transformations import rotate_about_point
 
 
 positions = np.array([[2., 2., 0.], [4., 2., 0.], [4., 0., 0.]])
