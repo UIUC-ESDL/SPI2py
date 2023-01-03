9ddbd0de540df3d758289165d34bb2f86b47c149
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-03 13:58:56 -0600

    Begin building Port class

---
+++ src/SPI2Py/data/objects/common.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+
+class Port:
+
+    def __init__(self, name, color, location, num_connections):
+        self.name = name
+        self.color = color
+        self.location = location
+        self.num_connections = num_connections
+
