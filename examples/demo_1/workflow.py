4823bf63961b470335ecc19146012da2f055ed23
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-03 12:49:10 -0600

    Refactor objects

--- examples/demo_1/workflow.py
+++ examples/demo_1/workflow.py
@@ -5,18 +5,14 @@ TODO Add a logger
 Note: Make sure to run this from the top-level SPI2Py directory
 """
 
-
 # Import the Application
-# First, we must import the application. When the application is done it will be hosted privately or publicly
-# and you'll be able to do some sort of "pip install" command. However, for the time being we must manually import it.
-# This requires us to add the project's filepath...
-
 from pathlib import Path
 import sys
 
-# Add the top-level project directory to the system path
-mypath = str(Path(__file__).parent.parent.parent)
-sys.path.append(mypath)
+import numpy as np
+
+SPI2py_path = str(Path(__file__).parent.parent.parent)
+sys.path.append(SPI2py_path)
 
 from src.SPI2Py.main import SPI2
 
@@ -24,7 +20,7 @@ from src.SPI2Py.main import SPI2
 # logging.basicConfig(filename='examples/demo_1/output.log', encoding='utf-8', level=logging.DEBUG)
 
 # For troubleshooting
-import numpy as np
+
 
 # Initialize the class
 demo = SPI2()
@@ -35,18 +31,16 @@ demo = SPI2()
 input_filepath = 'examples/demo_1/input.yaml'
 demo.add_input_file(input_filepath)
 
-
 # Specify the config file
 config_filepath = 'examples/demo_1/config.yaml'
 demo.add_configuration_file(config_filepath)
-
 
 # Generate classes from the inputs file
 demo.create_objects_from_input()
 
 # Map the objects to a 3D layout
 layout_generation_method = 'manual'
-locations = np.array([-3., -4.41, -0.24, 0.,  0., 0.,  2., 4.41, 0.24, 0., 0., 0., -1., 2., 2.])
+locations = np.array([-3., -4.41, -0.24, 0., 0., 0., 2., 4.41, 0.24, 0., 0., 0., -1., 2., 2.])
 demo.generate_layout(layout_generation_method, inputs=locations)
 
 # For development: Plot initial layout
@@ -55,15 +49,12 @@ demo.layout.plot_layout()
 # Perform gradient-based optimization
 demo.optimize_spatial_configuration()
 
-
 # For development: Print Results
 print('Result:', demo.result)
-
 
 # For development: Plot the final layout to see the change
 demo.layout.set_positions(demo.result.x)
 demo.layout.plot_layout()
-
 
 # Write output file
 output_filepath = 'examples/demo_1/output.json'
--- src/SPI2Py/layout/spatial_configuration.py
+++ src/SPI2Py/layout/spatial_configuration.py
@@ -3,120 +3,9 @@ TODO Make sure that migrating from InterconnectSegment to Interconnect does not 
 """
 
 import numpy as np
-from itertools import product, combinations
+
+from ..data.objects.systems import System
 from ..result.visualization.visualization import plot
-
-
-class Subsystem:
-    """
-    Defines the non-spatial aspects of the system.
-
-    """
-
-    def __init__(self, components, interconnect_nodes, interconnect_segments, structures):
-        self.components = components
-        self.interconnect_nodes = interconnect_nodes
-        self.interconnect_segments = interconnect_segments
-        self.structures = structures
-
-    @property
-    def objects(self):
-        return self.components + self.interconnect_nodes + self.interconnect_segments + self.structures
-
-    @property
-    def design_vector_objects(self):
-        return self.components + self.interconnect_nodes
-
-    @property
-    def moving_objects(self):
-        return self.components + self.interconnect_nodes + self.interconnect_segments
-
-    @property
-    def nodes(self):
-        return [design_object.node for design_object in self.design_vector_objects]
-
-    @property
-    def edges(self):
-        return [interconnect.edge for interconnect in self.interconnect_segments]
-
-    def add_object(self):
-        # TODO Implement this function
-        pass
-
-    def remove_object(self):
-        # TODO Implement this function
-        pass
-
-    @property
-    def component_component_pairs(self):
-        """
-        TODO Write unit tests to ensure it creates the correct pairs
-
-        :return:
-        """
-
-        component_component_pairs = list(combinations(self.components, 2))
-
-        return component_component_pairs
-
-    @property
-    def component_interconnect_pairs(self):
-        """
-        TODO Write unit tests to ensure it creates the correct pairs
-
-        :return:
-        """
-
-        component_interconnect_pairs = list(product(self.components, self.interconnect_segments))
-
-        # Remove interconnects attached to components b/c ...
-        for component, interconnect in component_interconnect_pairs:
-
-            if component is interconnect.object_1 or component is interconnect.object_2:
-                component_interconnect_pairs.remove((component, interconnect))
-
-        return component_interconnect_pairs
-
-    @property
-    def interconnect_interconnect_pairs(self):
-        """
-        TODO Write unit tests to ensure it creates the correct pairs
-
-        :return:
-        """
-
-        # Create a list of all interconnect pairs
-        interconnect_interconnect_pairs = list(combinations(self.interconnect_segments, 2))
-
-        # Remove segments that share the same component
-        # This is a temporary feature b/c the ends of those two interconnects will share a point
-        # and thus always overlap slightly...
-        # TODO implement this feature
-
-        # Remove segments that are from the same interconnect
-        # If a pipe intersects itself, usually the pipe can just be made shorter...
-        # TODO implement this feature
-
-        return interconnect_interconnect_pairs
-
-    @property
-    def structure_moving_object_pairs(self):
-        """
-        TODO Write unit tests to ensure it creates the correct pairs
-
-        :return:
-        """
-
-        structure_moving_object_pairs = list(product(self.structures, self.moving_objects))
-
-        return structure_moving_object_pairs
-
-
-class System(Subsystem):
-    """
-    System combines subsystems from various disciplines (e.g., fluid, electrical, structural)
-    """
-    pass
 
 
 class SpatialConfiguration(System):
@@ -238,3 +127,16 @@ class SpatialConfiguration(System):
 
         plot(layout_plot_array, savefig, directory)
 
+
+class Volume:
+    """
+    A class that captures the 3D space that we place objects in and optimize
+    """
+    pass
+
+
+class Volumes(Volume):
+    """
+    A class that combines contiguous volumes together.
+    """
+    pass
