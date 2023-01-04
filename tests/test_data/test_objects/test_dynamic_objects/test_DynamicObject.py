a38c29994f8ae5c1650fe6a549533a93d08d0fea
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-03 19:39:45 -0600

    Begin adding tests

---
+++ tests/test_data/test_objects/test_dynamic_objects/test_DynamicObject.py
@@ -0,0 +1,28 @@
+from src.SPI2Py.data.objects.dynamic_objects import DynamicObject
+
+# TODO Implement
+def test_create_DynamicObject():
+    pass
+
+def test_translate_only():
+    pass
+
+def test_rotate_only():
+    pass
+
+def test_translate_rotate():
+    pass
+
+# TODO Implement tests for constrained movement
+
+
+
+# Create an interconnect node
+
+# Create an interconnect segment
+
+# Create an interconnect with no interconnect nodes
+
+# Create an interconnect with one interconnect node
+
+# Create an interconnect with multiple interconnect nodes
