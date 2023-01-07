641d1e0b08221b53f2c224890b073c2ce8cc6628
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-06 19:15:48 -0600

    Figured out how to fix import statements, etc.

--- examples/demo_1/workflow.py
+++ examples/demo_1/workflow.py
@@ -2,9 +2,11 @@
 Example 1:  Simple optimization of a 3D layout
 Author:     Chad Peterson
 
+Notes:
+
 """
 
-# %% Run in Python Interactive
+# %% Optional: Run in Python Interactive
 
 # Import packages
 import os
@@ -13,51 +15,53 @@ import numpy as np
 from SPI2Py.main import SPI2
 
 # Get the system path for reading/writing example files
-cwd = os.getcwd() + '/examples/demo_1/'
+# Current working directory depends on how the script is run (e.g., Python or Python Interactive)
+cwd = os.getcwd() 
+if 'examples' not in cwd:
+    cwd += '/examples/demo_1/'
+else:
+    cwd += '/'
+    
 
 # Set up logging
-# logging.basicConfig(filename= cwd + 'output.log', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename= cwd + 'output.log', encoding='utf-8', level=logging.DEBUG)
 
 # SPI2 Workflow
 
 # Initialize the class
 demo = SPI2()
 
-a = 1
+# Specify the input file
+# input_filepath = cwd + '/input_1.yaml'
+input_filepath = cwd + 'input_2.yaml'
+demo.add_input_file(input_filepath)
 
-# # Specify the input file
-# # input_filepath = cwd + 'input_1.yaml'
-# input_filepath = cwd + 'input_2.yaml'
-# demo.add_input_file(input_filepath)
+# Specify the config file
+config_filepath = cwd + 'config.yaml'
+demo.add_configuration_file(config_filepath)
 
-# # Specify the config file
-# config_filepath = cwd + 'config.yaml'
-# demo.add_configuration_file(config_filepath)
+# Generate classes from the inputs file
+demo.create_objects_from_input()
 
-# # Generate classes from the inputs file
-# demo.create_objects_from_input()
+# Map the objects to a 3D layout
+layout_generation_method = 'manual'
+locations = np.array([-3., -4.41, -0.24, 0., 0., 0., 2., 4.41, 0.24, 0., 0., 0., -1., 2., 2.])
+demo.generate_layout(layout_generation_method, inputs=locations)
 
-# # Map the objects to a 3D layout
-# layout_generation_method = 'manual'
-# locations = np.array([-3., -4.41, -0.24, 0., 0., 0., 2., 4.41, 0.24, 0., 0., 0., -1., 2., 2.])
-# demo.generate_layout(layout_generation_method, inputs=locations)
+# For development: Plot initial layout
+demo.layout.plot_layout()
 
-# # For development: Plot initial layout
-# demo.layout.plot_layout()
+# Perform gradient-based optimization
+demo.optimize_spatial_configuration()
 
-# # Perform gradient-based optimization
-# demo.optimize_spatial_configuration()
+# For development: Print Results
+print('Result:', demo.result)
 
-# # For development: Print Results
-# print('Result:', demo.result)
+# For development: Plot the final layout to see the change
+demo.layout.set_positions(demo.result.x)
+demo.layout.plot_layout()
 
-# # For development: Plot the final layout to see the change
-# demo.layout.set_positions(demo.result.x)
-# demo.layout.plot_layout()
+# Write output file
+output_filepath = cwd + 'output.json'
+demo.write_output(output_filepath)
 
-# # Write output file
-# output_filepath = cwd + 'output.json'
-# demo.write_output(output_filepath)
-
-
-# %%
