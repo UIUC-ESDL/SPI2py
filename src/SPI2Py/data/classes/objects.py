21928c8e7cb890221591afbc367a0d838c90e27d
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-18 13:08:36 -0600

    Remove debugging code snippet

--- src/SPI2Py/data/classes/objects.py
+++ src/SPI2Py/data/classes/objects.py
@@ -51,16 +51,6 @@ class Port:
         self.location = location
         self.num_connections = num_connections
 
-# Define single port information
-port_nodes_single = [0]
-port_names_single = ['supply']
-port_colors_single = ['blue']
-port_locations_single = [[1, 2, 1]]
-port_number_of_connections_single = [1]
-
-
-port0 = Port(port_nodes_single[0], port_names_single[0], port_colors_single[0], port_locations_single[0], port_number_of_connections_single[0])
-
 class Component(DynamicObject):
 
     def __init__(self, positions, radii, color, node, name, movement=('3D Translation', '3D Rotation'),
