599406ff381050d179ce9b4a6d022deac7d75372
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-30 22:25:41 -0600

    ommit before merge

--- src/SPI2Py/data/spherical_decomposition.py
+++ src/SPI2Py/data/spherical_decomposition.py
@@ -45,6 +45,48 @@ def generate_rectangular_prism(origin, dimension):
 
     return positions, radii
 
+def generate_rectangular_prism2(origin, dimension):
+    """
+    Generates...
+
+    This is a rough representation, it doesn't actually package spheres precisely...
+    :param origin:
+    :param dimension:
+    :return:
+    """
+
+    # Recursion depth
+    rdepth = 2
+
+    # Set the diameter of the packing sphere to the smallest dimension
+    diameter = np.min(dimension)
+    radius = diameter / 2
+
+    origin_x, origin_y, origin_z = origin
+    len_x, len_y, len_z = dimension
+
+    num_x_spheres = int(len_x // diameter)
+    num_y_spheres = int(len_y // diameter)
+    num_z_spheres = int(len_z // diameter)
+
+    # Add 2 since we want to fit the spheres inside the box, not along its corners
+    num_x_nodes = num_x_spheres + 2
+    num_y_nodes = num_y_spheres + 2
+    num_z_nodes = num_z_spheres + 2
+
+    # [1:-1] Removes the first and last nodes because...
+    x = np.linspace(origin_x, origin_x + len_x, num_x_nodes)[1:-1]
+    y = np.linspace(origin_y, origin_y + len_y, num_y_nodes)[1:-1]
+    z = np.linspace(origin_z, origin_z + len_z, num_z_nodes)[1:-1]
+
+    xx, yy, zz = np.meshgrid(x, y, z)
+
+    positions = np.hstack((xx.reshape(-1, 1), yy.reshape(-1, 1), zz.reshape(-1, 1)))
+
+    sphere_count = positions.shape[0]
+    radii = np.repeat(radius, sphere_count)
+
+    return positions, radii
 
 def generate_rectangular_prisms(origins, dimensions):
 
