84f1fccb546706ad2dc9a19f4e6a90a4ee2e35f3
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-03-07 11:03:26 -0600

    Add TODOs for constraint aggregation methods

---
+++ src/SPI2py/analysis/constraint_aggregation.py
@@ -0,0 +1,8 @@
+"""
+
+TODO Implement the Kreisselmeier-Steinhauser (KS) method for constraint aggregation.
+TODO Implmenent the alternative Kreisselmeier-Steinhauser (KS) method for constraint aggregation.
+TODO Implement the p-norm method for constraint aggregation.
+TODO Implement the induced exponential method for constraint aggregation.
+TODO Implement the induced power function method for constraint aggregation.
+"""
