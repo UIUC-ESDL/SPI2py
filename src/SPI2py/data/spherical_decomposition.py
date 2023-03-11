019f5182ea6116765415cd46d455783797bf6670
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-03-11 14:25:03 -0600

    Beginning to work, needs tweaking

--- src/SPI2py/data/spherical_decomposition.py
+++ src/SPI2py/data/spherical_decomposition.py
@@ -128,96 +128,191 @@ ax = fig.add_subplot(projection='3d')
 
 ax.scatter(rect[:, 0], rect[:, 1], rect[:, 2], marker='o', s=100, c='r')
 
+#
+#
+# def run_optimization(xrange, yrange, zrange, recursion_depth=6, max_iter=20):
+#
+#     # Generate the initial shape
+#     x1, x2 = xrange
+#     y1, y2 = yrange
+#     z1, z2 = zrange
+#
+#     shape = np.array([[x1, y1, z1],
+#                       [x1, y2, z1],
+#                       [x1, y1, z2],
+#                       [x1, y2, z2],
+#                       [x2, y1, z2],
+#                       [x2, y1, z1],
+#                       [x2, y2, z1],
+#                       [x2, y2, z2]])
+#
+#     points = np.empty((0, 3))
+#     radii = np.empty(0)
+#
+#     def objective(di): return di[3]
+#
+#     def constraint_1(di):
+#         """Min dis must be > 0 """
+#
+#         x, y, z, r = di
+#
+#         point = np.array([[x, y, z]])
+#         radius = np.array([r])
+#
+#         if points.shape[0] == 0:
+#             return min([x2-x1, y2-y1, z2-z1])/2
+#
+#         else:
+#
+#             gaps = []
+#
+#             for j in range(len(points)):
+#                 gap = np.linalg.norm(point - points[j]) - (radius + radii[j])
+#                 gaps.append(gap)
+#
+#             return min(gaps)
+#
+#     d0 = np.array([0.5, 0.5, 0.5, 0.25])
+#
+#     g1 = NonlinearConstraint(constraint_1, 0, np.inf)
+#
+#     # x > x1
+#     g2 = NonlinearConstraint(lambda di: di[0], x1, np.inf)
+#
+#     # x < x2
+#     g3 = NonlinearConstraint(lambda di: di[0], -np.inf, x2)
+#
+#     # y > y1
+#     g4 = NonlinearConstraint(lambda di: di[1], y1, np.inf)
+#
+#     # y < y2
+#     g5 = NonlinearConstraint(lambda di: di[1], -np.inf, y2)
+#
+#     # z > z1
+#     g6 = NonlinearConstraint(lambda di: di[2], z1, np.inf)
+#
+#     # z < z2
+#     g7 = NonlinearConstraint(lambda di: di[2], -np.inf, z2)
+#
+#     max_r = min([x2-x1, y2-y1, z2-z1])/2
+#
+#     # TODO Implement bounds
+#     bounds = Bounds([x1, y1, z1, 0], [x2, y2, z2, max_r])
+#
+#     for i in range(recursion_depth):
+#         # d0 is random
+#         res = minimize(objective, d0, method='trust-constr', constraints=[g1, g2, g3, g4, g5, g6, g7], bounds=bounds)
+#         # print(res)
+#         points = np.vstack((points, res.x[:3]))
+#         radii = np.append(radii, res.x[3])
+#         print('point', res.x[:3])
+#         print('radius', res.x[3])
+#
+#     return points, radii
+#
+#
+# points_opt, radii_opt = run_optimization([0, 1], [0, 1], [0, 1])
+#
+# ax.scatter(points_opt[:, 0], points_opt[:, 1], points_opt[:, 2], marker='o', s=100, c='b')
+#
+# plt.show()
 
 
-def run_optimization(xrange, yrange, zrange, recursion_depth=6, max_iter=20):
-
-    # Generate the initial shape
-    x1, x2 = xrange
-    y1, y2 = yrange
-    z1, z2 = zrange
-
-    shape = np.array([[x1, y1, z1],
-                      [x1, y2, z1],
-                      [x1, y1, z2],
-                      [x1, y2, z2],
-                      [x2, y1, z2],
-                      [x2, y1, z1],
-                      [x2, y2, z1],
-                      [x2, y2, z2]])
-
-    points = np.empty((0, 3))
-    radii = np.empty(0)
-
-    def objective(d):
-        x, y, z, r = d
-
-        point = np.array([[x, y, z]])
-        all_points = np.vstack((shape, points))
-
-        # Find the minimum distance between the point and all other points
-        min_dist = float(np.min(np.linalg.norm(point - all_points)))
-
-        return min_dist
-
-    def constraint_1(di):
-        """Min dis must be > 0 """
-
-        x, y, z, r = di
-
-        point = np.array([[x, y, z]])
-        radius = np.array([r])
-
-        if points.shape[0] == 0:
-            return min([x2-x1, y2-y1, z2-z1])/2
-
-        else:
-
-            gaps = []
-
-            for j in range(len(points)):
-                gap = (radius + radii[j]) - np.linalg.norm(point - points[j])
-                gaps.append(gap)
-
-            return min(gaps)
-
-    d0 = np.array([0, 0, 0, 0.25])
-
-    g1 = NonlinearConstraint(constraint_1, 0, np.inf)
-
-    # x > x1
-    g2 = NonlinearConstraint(lambda di: di[0], x1, np.inf)
-
-    # x < x2
-    g3 = NonlinearConstraint(lambda di: di[0], -np.inf, x2)
-
-    # y > y1
-    g4 = NonlinearConstraint(lambda di: di[1], y1, np.inf)
-
-    # y < y2
-    g5 = NonlinearConstraint(lambda di: di[1], -np.inf, y2)
-
-    # z > z1
-    g6 = NonlinearConstraint(lambda di: di[2], z1, np.inf)
-
-    # z < z2
-    g7 = NonlinearConstraint(lambda di: di[2], -np.inf, z2)
-
-    # TODO Implement bounds
-    bounds = Bounds([0, 0, 0, 0], [1, 1, 1, 1])
-
-    for i in range(recursion_depth):
-        # d0 is random
-        res = minimize(objective, d0, method='trust-constr', constraints=[g1, g2, g3, g4, g5, g6, g7], bounds=bounds)
-        print(res)
-        points = np.vstack((points, res.x[:3]))
-        radii = np.append(radii, res.x[3])
-
-    return points, radii
 
 
-points_opt, radii_opt = run_optimization([0, 1], [0, 1], [0, 1])
 
-ax.scatter(points_opt[:, 0], points_opt[:, 1], points_opt[:, 2], marker='o', s=100, c='b')
+xrange = [0, 1]
+yrange = [0, 1]
+zrange = [0, 1]
+
+
+
+# Generate the initial shape
+x1, x2 = xrange
+y1, y2 = yrange
+z1, z2 = zrange
+
+max_r = min([x2-x1, y2-y1, z2-z1])/2
+
+shape = np.array([[x1, y1, z1],
+                  [x1, y2, z1],
+                  [x1, y1, z2],
+                  [x1, y2, z2],
+                  [x2, y1, z2],
+                  [x2, y1, z1],
+                  [x2, y2, z1],
+                  [x2, y2, z2]])
+
+points = np.empty((0, 3))
+radii = np.empty(0)
+
+def objective(di): return 1/di[3]
+
+def constraint_1(di):
+    """Min dis must be > 0 """
+
+    x, y, z, r = di
+
+    point = np.array([[x, y, z]])
+    radius = np.array([r])
+
+    if points.shape[0] == 0:
+        return float(min([x2-x1, y2-y1, z2-z1])/2)
+
+    else:
+
+        gaps = []
+
+        for j in range(len(points)):
+            gap = np.linalg.norm(point - points[j]) - (radius + radii[j])
+            gaps.append(gap)
+
+        return float(min(gaps))
+
+g1 = NonlinearConstraint(constraint_1, 0, np.inf)
+
+# x > x1
+g2 = NonlinearConstraint(lambda di: di[0], x1, np.inf)
+
+# x < x2
+g3 = NonlinearConstraint(lambda di: di[0], -np.inf, x2)
+
+# y > y1
+g4 = NonlinearConstraint(lambda di: di[1], y1, np.inf)
+
+# y < y2
+g5 = NonlinearConstraint(lambda di: di[1], -np.inf, y2)
+
+# z > z1
+g6 = NonlinearConstraint(lambda di: di[2], z1, np.inf)
+
+# z < z2
+g7 = NonlinearConstraint(lambda di: di[2], -np.inf, z2)
+
+
+
+# TODO Implement bounds
+bounds = Bounds([x1, y1, z1, 0], [x2, y2, z2, max_r])
+
+for i in range(20):
+
+    # TODO Make this work for other than 0 to 1
+    d0 = np.random.rand(4)
+
+    # d0 = np.array([0.5, 0.5, 0.5, 0.25])
+
+    # d0 is random
+    res = minimize(objective, d0, method='trust-constr', constraints=[g1, g2, g3, g4, g5, g6, g7], bounds=bounds)
+    # print(res)
+    points = np.vstack((points, res.x[:3]))
+    radii = np.append(radii, res.x[3])
+    print('point', res.x[:3])
+    print('radius', res.x[3])
+
+ax.scatter(points[:, 0], points[:, 1], points[:, 2], marker='o', s=300, c='b')
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
+ax.set_zlim(-2, 2)
+
 
 plt.show()
