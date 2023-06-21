294ef6a8e21ebd52cca7e3a83b20981c93daeeda
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-06-21 16:30:31 -0500

    Placeholder commit

--- src/SPI2py/group_model/component_spatial/spatial_transformations.py
+++ src/SPI2py/group_model/component_spatial/spatial_transformations.py
@@ -107,9 +107,107 @@ def rigid_transformation(reference_position, positions, x, y, z, rx, ry, rz, ref
 
     return rotated_positions
 
+# def rigid_transformation(reference_position, positions, x, y, z, rx, ry, rz, reference_axes='origin'):
+#     """
+#     Apply translation and rotation to an object.
 
-def test_rotate():
-    data = np.array(...)
-    answer=runfunction(data)
+#     TODO Evaluate lower dimensional representations, e.g. screw
+#     <https://en.wikipedia.org/wiki/Chasles%27_theorem_(kinematics)>
 
-    assert answer == expected_answer_calculatedbyhand
+#     TODO Add support for non-origin coordinate systems (e.g., object dependencies)
+#     """
+
+#     # Convert rotation angles to radians
+#     rx = np.radians(rx)
+#     ry = np.radians(ry)
+#     rz = np.radians(rz)
+
+#     # Calculate rotation matrix
+#     R_x = np.array([[1, 0, 0], [0, np.cos(rx), -np.sin(rx)], [0, np.sin(rx), np.cos(rx)]])
+#     R_y = np.array([[np.cos(ry), 0, np.sin(ry)], [0, 1, 0], [-np.sin(ry), 0, np.cos(ry)]])
+#     R_z = np.array([[np.cos(rz), -np.sin(rz), 0], [np.sin(rz), np.cos(rz), 0], [0, 0, 1]])
+#     R = np.dot(R_z, np.dot(R_y, R_x))
+
+#     # Create homogeneous transformation matrix
+#     T = np.eye(4)
+#     T[:3, :3] = R
+#     T[:3, 3] = [x, y, z]
+
+#     # Apply transformation
+#     transformed_positions = np.dot(T, np.vstack((positions.T, np.ones(len(positions))))).T[:, :3]
+
+#     return transformed_positions
+
+
+# def rigid_transformation(reference_position, positions, x, y, z, rx, ry, rz, reference_axes='origin'):
+#     # Convert rotation angles to radians
+#     rx = np.radians(rx)
+#     ry = np.radians(ry)
+#     rz = np.radians(rz)
+
+#     # Calculate rotation matrix
+#     R_x = np.array([[1, 0, 0],
+#                     [0, np.cos(rx), -np.sin(rx)],
+#                     [0, np.sin(rx), np.cos(rx)]])
+#     R_y = np.array([[np.cos(ry), 0, np.sin(ry)],
+#                     [0, 1, 0],
+#                     [-np.sin(ry), 0, np.cos(ry)]])
+#     R_z = np.array([[np.cos(rz), -np.sin(rz), 0],
+#                     [np.sin(rz), np.cos(rz), 0],
+#                     [0, 0, 1]])
+#     R = R_z @ R_y @ R_x
+
+#     # Create homogeneous transformation matrix
+#     T = np.eye(4)
+#     T[:3, :3] = R
+#     T[:3, 3] = [x, y, z]
+
+#     # Apply transformation
+#     positions_homogeneous = np.hstack((positions, np.ones((len(positions), 1))))
+#     transformed_positions_homogeneous = positions_homogeneous @ T.T
+#     transformed_positions = transformed_positions_homogeneous[:, :3]
+
+#     return transformed_positions
+
+
+
+# def test_translation_only():
+#     # Test translation only
+#     reference_position = np.array([0, 0, 0])
+#     positions = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+#     x, y, z = 1, 2, 3
+#     rx, ry, rz = 0, 0, 0
+#     expected_transformed_positions = np.array([[2, 4, 6], [5, 7, 9], [8, 10, 12]])
+#     transformed_positions = rigid_transformation(reference_position, positions, x, y, z, rx, ry, rz)
+#     np.testing.assert_allclose(transformed_positions, expected_transformed_positions)
+
+# def test_rotation_only():
+#     # Test rotation only
+#     reference_position = np.array([0, 0, 0])
+#     positions = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+#     x, y, z = 0, 0, 0
+#     rx, ry, rz = 90, 0, 0
+#     expected_transformed_positions = np.array([[1, -3, 2], [4, -6, 5], [7, -9, 8]])
+#     transformed_positions = rigid_transformation(reference_position, positions, x, y, z, rx, ry, rz)
+#     np.testing.assert_allclose(transformed_positions, expected_transformed_positions)
+
+# def test_translation_and_rotation():
+#     # Test translation and rotation
+#     reference_position = np.array([0, 0, 0])
+#     positions = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+#     x, y, z = 1, 2, 3
+#     rx, ry, rz = 90, 0, 0
+#     expected_transformed_positions = np.array([[1, -1, 4], [4, -2, 7], [7, -3, 10]])
+#     transformed_positions = rigid_transformation(reference_position, positions, x, y, z, rx, ry, rz)
+#     np.testing.assert_allclose(transformed_positions, expected_transformed_positions)
+
+
+# def test_translation_and_rotation():
+#     # Test translation and rotation
+#     reference_position = np.array([0, 0, 0])
+#     positions = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+#     x, y, z = 1, 2, 3
+#     rx, ry, rz = 90, 90, 90
+#     expected_transformed_positions = np.array([[-2, 4, 8], [-5, 7, 10], [-8, 10, 12]])
+#     transformed_positions = rigid_transformation(reference_position, positions, x, y, z, rx, ry, rz)
+#     np.testing.assert_allclose(transformed_positions, expected_transformed_positions)
