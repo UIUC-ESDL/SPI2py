c72cee4033c9865ada125d92202bd72720c02754
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-06-22 17:20:45 -0500

    Improving pseudo-MDBD

--- examples/prototyping/pseudoMDBD.py
+++ examples/prototyping/pseudoMDBD.py
@@ -18,34 +18,37 @@ from scipy.optimize import minimize, NonlinearConstraint, Bounds
 import matplotlib.pyplot as plt
 
 
+def objective(d_i):
+    """Maximize radius of the sphere by minimizing the negative radius"""
+
+    _, _, _, ri = d_i
+
+    return -ri
+
+
+def constraint(d_i):
+    """The distance from the sphere to the surface must be greater than the radius"""
+
+    xi, yi, zi, ri = d_i
+
+    point = np.array([[xi, yi, zi]])
+
+    min_distance = trimesh.proximity.signed_distance(mesh, point)
+
+    return ri - min_distance
+
 # Load file
 
 filepath = 'C:/Users/cpgui/PycharmProjects/SPI2py/examples/prototyping/files/part2.stl'
 
-# Load the file
-# mesh = pv.read(filepath)
-# cpos = mesh.plot()
-# grid = pv.read(filepath)
-#
-# centers = grid.cell_centers()
-#
-# pl = pv.Plotter()
-# pl.add_mesh(grid.extract_all_edges(), color="k", line_width=1)
-# pl.add_mesh(centers, color="r", point_size=8.0, render_points_as_spheres=True)
-# pl.show()
 
 mesh = trimesh.exchange.load.load(filepath)
 
-# mesh.show()
-# See trimesh.proximity.max_tangent_sphere
-
-
+# Find the first point
 
 p1 = np.array([[5, 5, 5]])
 
 min_dist = trimesh.proximity.signed_distance(mesh, p1)
-
-
 
 # Define variable bounds
 
@@ -56,8 +59,7 @@ y_max = mesh.vertices[:, 1].max()
 z_min = mesh.vertices[:, 2].min()
 z_max = mesh.vertices[:, 2].max()
 r_min = 0.01
-r_max = min([x_max-x_min, y_max-y_min, z_max-z_min])/2
-
+r_max = min([x_max - x_min, y_max - y_min, z_max - z_min]) / 2
 
 # Create an initial population of sample points
 nx = 12
@@ -89,98 +91,35 @@ max_min_distance = max(min_distances)
 max_min_point = points[np.argmax(min_distances)]
 
 
-#
-# # Initialize empty arrays
-# points = np.empty((0, 3))
-# radii = np.empty(0)
-#
-#
-# def objective(d_i):
-#     """Maximize radius of the sphere by minimizing the negative radius"""
-#
-#     x, y, z, r = d_i
-#
-#     return -r
-#
-#
-# def constraint_1(d_i):
-#     """The signed distance of the sphere must be greater than the radius of the sphere.
-#
-#     Note: The sign of dist is flipped to make it a constraint since the trimesh convention is that the sign is positive
-#     if the point is inside the mesh.
-#     """
-#
-#     x, y, z, r = d_i
-#
-#     point = np.array([[x, y, z]])
-#
-#     dist = trimesh.proximity.signed_distance(mesh, point)
-#
-#
-#     dist = -float(dist)
-#
-#     dist_min = dist - r
-#
-#     return dist_min
-#
-# def constraint_2(di):
-#     """The sphere must not overlap with existing spheres"""
-#
-#     x, y, z, r = di
-#
-#     point = np.array([[x, y, z]])
-#     radius = np.array([r])
-#
-#     if points.shape[0] == 0:
-#         overlap = -10 # Arbitrary negative number
-#
-#     else:
-#
-#         gaps = []
-#
-#         for k in range(len(points)):
-#             gap = np.linalg.norm(point - points[k]) - (radius + radii[k])
-#             gaps.append(gap)
-#
-#         overlap = float(min(gaps))
-#
-#     return overlap
-#
-#
-#
-# bounds = Bounds([x_min, y_min, z_min, r_min], [x_max, y_max, z_max, r_max])
-# nlc_1 = NonlinearConstraint(constraint_1, -np.inf, 0)
-# nlc_2 = NonlinearConstraint(constraint_2, -np.inf, 0)
-#
-#
-# for i in range(1):
-#
-#     d = []
-#     f = []
-#     for j in range(5):
-#         d0 = np.random.rand(4)
-#         res = minimize(objective, d0,
-#                        method='trust-constr',
-#                        constraints=nlc_1,
-#                        bounds=bounds,
-#                        tol=1e-1)
-#         d.append(res.x)
-#         f.append(res.fun)
-#
-#     d_opt = d[np.argmax(f)]
-#
-#     points = np.vstack((points, d_opt[:3]))
-#     radii = np.append(radii, d_opt[3])
-#     print('point', d_opt[:3])
-#     print('radius', d_opt[3])
-#
-#
+
+
+
+bounds = Bounds([x_min, y_min, z_min, r_min], [x_max, y_max, z_max, r_max])
+nlc = NonlinearConstraint(constraint, -np.inf, 2)
+
+x_0, y_0, z_0 = max_min_point
+r_0 = 0.5 * max_min_distance[0]
+
+d_0 = np.array([x_0, y_0, z_0, r_0])
+
+res = minimize(objective, d_0,
+               method='trust-constr',
+               constraints=nlc,
+               bounds=bounds,
+               tol=1e-3)
+
+
+
+
+
+
+
+
 # Plot object with PyVista
 plotter = pv.Plotter()
 
 part2 = pv.read(filepath)
 plotter.add_mesh(part2, color='white', opacity=0.5)
-
 
 # Plot points
 points = pv.PolyData(points)
@@ -190,8 +129,43 @@ plotter.add_mesh(points, color='red', point_size=10, render_points_as_spheres=Tr
 max_min_point = pv.PolyData(max_min_point)
 plotter.add_mesh(max_min_point, color='blue', point_size=20, render_points_as_spheres=True)
 
+
+
+# Plot the sphere
+sphere = pv.Sphere(center=res.x[:3], radius=res.x[3])
+plotter.add_mesh(sphere, color='green', opacity=0.75)
+
 # for i in range(len(points)):
 #     sphere = pv.Sphere(center=points[i], radius=radii[i])
 #     plotter.add_mesh(sphere, color='red', opacity=0.75)
 #
 plotter.show()
+
+
+
+
+sphere = pv.Sphere(center=res.x[:3], radius=res.x[3])
+# sphere = pv.Sphere(radius=50, center=(15, 15, 15))
+# result = cube.boolean_difference(sphere)
+result = part2.boolean_difference(sphere)
+result.plot(color='tan')
+
+# sphere = pv.Sphere(center=res.x[:3], radius=res.x[3]).triangulate().subdivide(3)
+#
+# result = part2.boolean_difference(sphere)
+# result.plot
+
+
+# plotter = pv.Plotter()
+
+# Create trimesh for sphere
+# sphere = trimesh.primitives.Sphere(center=res.x[:3], radius=res.x[3], subdivisions=4)
+# sphere = trimesh.creation.uv_sphere(radius=res.x[3], transform=trimesh.transformations.translation_matrix(res.x[:3]))
+
+# Remove the sphere from the mesh
+# result = trimesh.boolean.difference(mesh, sphere)
+
+# # Plot the mesh
+# plotter.add_mesh(mesh, color='white', opacity=0.5)
+#
+# plotter.show()
