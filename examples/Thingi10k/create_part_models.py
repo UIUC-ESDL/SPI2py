b86cdf0461e2b4a2c37a98995e347246c07931b4
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-10-03 16:11:37 -0500

    Adding MDBD cad files and stuff

--- examples/Thingi10k/create_part_models.py
+++ examples/Thingi10k/create_part_models.py
@@ -4,7 +4,7 @@
 import os
 from SPI2py.group_model.component_geometry.spherical_decomposition_methods.maximal_disjoint_ball_decomposition import mdbd
 
-filenames = os.listdir(path='Thingi10k_models/')
+# filenames = os.listdir(path='Thingi10k_models/')
 
 num_spheres = 1000
 min_radius = 0.01
@@ -15,8 +15,17 @@ increments = 40
 
 
 
-mdbd('', 'poets/MarqueeAlphabet_P_fixed.stl', 'poets/MarqueeAlphabet_P_fixed.xyzr', color='#1873BA', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
-mdbd('', 'poets/MarqueeAlphabet_O_fixed.stl', 'poets/MarqueeAlphabet_O_fixed.xyzr', color='#F3D316', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
-mdbd('', 'poets/MarqueeAlphabet_E_fixed.stl', 'poets/MarqueeAlphabet_E_fixed.xyzr', color='#F28223', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
-mdbd('', 'poets/MarqueeAlphabet_T_fixed.stl', 'poets/MarqueeAlphabet_T_fixed.xyzr', color='#D02029', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
-mdbd('', 'poets/MarqueeAlphabet_S_fixed.stl', 'poets/MarqueeAlphabet_S_fixed.xyzr', color='#363636', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
+# mdbd('', 'poets/MarqueeAlphabet_P_fixed.stl', 'poets/MarqueeAlphabet_P_fixed.xyzr', color='#1873BA', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
+# mdbd('', 'poets/MarqueeAlphabet_O_fixed.stl', 'poets/MarqueeAlphabet_O_fixed.xyzr', color='#F3D316', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
+# mdbd('', 'poets/MarqueeAlphabet_E_fixed.stl', 'poets/MarqueeAlphabet_E_fixed.xyzr', color='#F28223', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
+# mdbd('', 'poets/MarqueeAlphabet_T_fixed.stl', 'poets/MarqueeAlphabet_T_fixed.xyzr', color='#D02029', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
+# mdbd('', 'poets/MarqueeAlphabet_S_fixed.stl', 'poets/MarqueeAlphabet_S_fixed.xyzr', color='#363636', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
+
+
+# mdbd('', 'cad_models/engine.stl', 'cad_models/engine.xyzr', color='#1873BA', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
+# mdbd('', 'poets/MarqueeAlphabet_O_fixed.stl', 'poets/MarqueeAlphabet_O_fixed.xyzr', color='#F3D316', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
+# mdbd('', 'poets/MarqueeAlphabet_E_fixed.stl', 'poets/MarqueeAlphabet_E_fixed.xyzr', color='#F28223', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
+# mdbd('', 'poets/MarqueeAlphabet_T_fixed.stl', 'poets/MarqueeAlphabet_T_fixed.xyzr', color='#D02029', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
+
+
+mdbd('', 'Thingi10k_models/stepper_gear.stl', 'Thingi10k_models/stepper_gear.xyzr', color='#1873BA', num_spheres=num_spheres, min_radius=min_radius, meshgrid_increment=increments)
