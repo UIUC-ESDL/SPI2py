ceceb7dd4760675e37e911c81cc95b41c8bf196e
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2024-03-04 20:10:00 -0600

    Adding components and interconnects back in

--- examples/Ford_AFCS_Replication/workflow.py
+++ examples/Ford_AFCS_Replication/workflow.py
@@ -10,6 +10,7 @@ from SPI2py.API.constraints import PairwiseCollisionDetection
 from SPI2py.API.utilities import Multiplexer, MaxAggregator
 from SPI2py.models.utilities.visualization import plot_problem
 from SPI2py.models.utilities.inputs import read_input_file
+from itertools import combinations, product
 
 
 # Read the input file
@@ -20,71 +21,92 @@ prob = om.Problem()
 model = prob.model
 
 # Parameters
-n_components = 4
+n_components = 10
 n_spheres = 10
-n_spheres_per_object = [n_spheres, n_spheres, n_spheres, n_spheres]
-m_interconnects = 2
+n_spheres_per_object = [n_spheres for _ in range(n_components)]
+m_interconnects = 10
 m_segments = 2
 m_spheres_per_segment = 25
 m_spheres = m_segments * m_spheres_per_segment
-m_spheres_per_object = [m_spheres, m_spheres]
-
+m_spheres_per_object = [m_spheres for _ in range(m_interconnects)]
 
 
 # Initialize the groups
 model.add_subsystem('system', System(input_dict=input_file))
-# model.add_subsystem('mux_comp_sphere_positions', Multiplexer(n_i=n_spheres_per_object, m=3))
-# model.add_subsystem('mux_comp_sphere_radii', Multiplexer(n_i=n_spheres_per_object, m=1))
-# model.add_subsystem('mux_int_sphere_positions', Multiplexer(n_i=m_spheres_per_object, m=3))
-# model.add_subsystem('mux_int_sphere_radii', Multiplexer(n_i=m_spheres_per_object, m=1))
-# model.add_subsystem('mux_all_sphere_positions', Multiplexer(n_i=[sum(n_spheres_per_object), sum(m_spheres_per_object)], m=3))
-# model.add_subsystem('mux_all_sphere_radii', Multiplexer(n_i=[sum(n_spheres_per_object), sum(m_spheres_per_object)], m=1))
 model.add_subsystem('mux_all_sphere_positions', Multiplexer(n_i=n_spheres_per_object + m_spheres_per_object, m=3))
 model.add_subsystem('mux_all_sphere_radii', Multiplexer(n_i=n_spheres_per_object + m_spheres_per_object, m=1))
 model.add_subsystem('bbv', BoundingBoxVolume(n_spheres_per_object=n_spheres_per_object+m_spheres_per_object))
-#
-# # Add a Multiplexer for component sphere positions
-# for i in range(n_components):
-#     model.connect(f'system.components.comp_{i}.transformed_sphere_positions', f'mux_comp_sphere_positions.input_{i}')
-#
-# # Add a Multiplexer for component sphere radii
-# for i in range(n_components):
-#     model.connect(f'system.components.comp_{i}.transformed_sphere_radii', f'mux_comp_sphere_radii.input_{i}')
-#
-# # Add a Multiplexer for interconnect sphere positions and radii
-# for i in range(m_interconnects):
-#     model.connect(f'system.interconnects.int_{i}.transformed_positions', f'mux_int_sphere_positions.input_{i}')
-#
-# for i in range(m_interconnects):
-#     model.connect(f'system.interconnects.int_{i}.transformed_radii', f'mux_int_sphere_radii.input_{i}')
-
-# Mux components with interconnects
-
 
 
 # Add a Multiplexer for component sphere positions
-i=0
+i = 0
 for j in range(n_components):
     model.connect(f'system.components.comp_{j}.transformed_sphere_positions', f'mux_all_sphere_positions.input_{i}')
     model.connect(f'system.components.comp_{j}.transformed_sphere_radii', f'mux_all_sphere_radii.input_{i}')
-    i+=1
+    i += 1
 
 # Add a Multiplexer for interconnect sphere positions and radii
 for j in range(m_interconnects):
     model.connect(f'system.interconnects.int_{j}.transformed_positions', f'mux_all_sphere_positions.input_{i}')
     model.connect(f'system.interconnects.int_{j}.transformed_radii', f'mux_all_sphere_radii.input_{i}')
-    i+=1
-
-# model.connect('mux_comp_sphere_positions.stacked_output', 'mux_all_sphere_positions.input_0')
-# model.connect('mux_int_sphere_positions.stacked_output', 'mux_all_sphere_positions.input_1')
-# model.connect('mux_comp_sphere_radii.stacked_output', 'mux_all_sphere_radii.input_0')
-# model.connect('mux_int_sphere_radii.stacked_output', 'mux_all_sphere_radii.input_1')
+    i += 1
 
 model.connect('mux_all_sphere_positions.stacked_output', 'bbv.positions')
 model.connect('mux_all_sphere_radii.stacked_output', 'bbv.radii')
 
 
-# # Component-Component Collision Detection
+# Define the combinatorial collision detection pairs
+component_component_pairs = list(combinations(range(n_components), 2))
+interconnect_interconnect_pairs = list(combinations(range(m_interconnects), 2))
+component_interconnect_pairs = list(product(range(n_components), range(m_interconnects)))
+
+# Loop through the pairs and add the collision detection components
+for i, pair in enumerate(component_component_pairs):
+    collision = PairwiseCollisionDetection(n_spheres=n_spheres, m_spheres=n_spheres)
+    model.add_subsystem(f'collision_{i}', collision)
+    model.connect(f'system.components.comp_{pair[0]}.transformed_sphere_positions', f'collision_{i}.positions_a')
+    model.connect(f'system.components.comp_{pair[0]}.transformed_sphere_radii', f'collision_{i}.radii_a')
+    model.connect(f'system.components.comp_{pair[1]}.transformed_sphere_positions', f'collision_{i}.positions_b')
+    model.connect(f'system.components.comp_{pair[1]}.transformed_sphere_radii', f'collision_{i}.radii_b')
+
+# for i, pair in enumerate(interconnect_interconnect_pairs):
+#     collision = PairwiseCollisionDetection(n_spheres=m_spheres, m_spheres=m_spheres)
+#     model.add_subsystem(f'collision_{i}', collision)
+#     model.connect(f'system.interconnects.int_{pair[0]}.transformed_positions', f'collision_{i}.positions_a')
+#     model.connect(f'system.interconnects.int_{pair[0]}.transformed_radii', f'collision_{i}.radii_a')
+#     model.connect(f'system.interconnects.int_{pair[1]}.transformed_positions', f'collision_{i}.positions_b')
+#     model.connect(f'system.interconnects.int_{pair[1]}.transformed_radii', f'collision_{i}.radii_b')
+
+# for i, pair in enumerate(component_interconnect_pairs):
+#     collision = PairwiseCollisionDetection(n_spheres=n_spheres, m_spheres=m_spheres)
+#     model.add_subsystem(f'collision_{i}', collision)
+#     model.connect(f'system.components.comp_{pair[0]}.transformed_sphere_positions', f'collision_{i}.positions_a')
+#     model.connect(f'system.components.comp_{pair[0]}.transformed_sphere_radii', f'collision_{i}.radii_a')
+#     model.connect(f'system.interconnects.int_{pair[1]}.transformed_positions', f'collision_{i}.positions_b')
+#     model.connect(f'system.interconnects.int_{pair[1]}.transformed_radii', f'collision_{i}.radii_b')
+
+# Aggregate each collision detection output
+for i in range(len(component_component_pairs)):
+    collision_aggregation = MaxAggregator(n=n_spheres, m=n_spheres)
+    model.add_subsystem(f'collision_aggregation_{i}', collision_aggregation)
+    model.connect(f'collision_{i}.signed_distances', f'collision_aggregation_{i}.input_vector')
+
+# Multiplex the aggregated collision detection outputs
+collision_multiplexer = Multiplexer(n_i=[1]*len(component_component_pairs), m=1)
+model.add_subsystem('collision_multiplexer', collision_multiplexer)
+for i in range(len(component_component_pairs)):
+    model.connect(f'collision_aggregation_{i}.aggregated_output', f'collision_multiplexer.input_{i}')
+
+# Aggregate the multiplexed collision detection outputs
+total_collision_aggregation = MaxAggregator(n=len(component_component_pairs), m=1)
+model.add_subsystem('total_collision_aggregation', total_collision_aggregation)
+model.connect('collision_multiplexer.stacked_output', 'total_collision_aggregation.input_vector')
+
+
+
+
+
+
 # collision = PairwiseCollisionDetection(n_spheres=n_spheres, m_spheres=n_spheres)
 # model.add_subsystem('collision1', collision)
 # model.connect('system.components.comp_0.transformed_sphere_positions', 'collision1.positions_a')
@@ -124,7 +146,7 @@ model.connect('mux_all_sphere_radii.stacked_output', 'bbv.radii')
 # collision_aggregation3 = MaxAggregator(n=n_spheres, m=n_spheres)
 # model.add_subsystem('collision_aggregation3', collision_aggregation3)
 # model.connect('collision3.signed_distances', 'collision_aggregation3.input_vector')
-#
+# #
 # # collision_aggregation4 = MaxAggregator(n=n_spheres, m=m_spheres)
 # # model.add_subsystem('collision_aggregation4', collision_aggregation4)
 # # model.connect('collision4.signed_distances', 'collision_aggregation4.input_vector')
@@ -134,7 +156,7 @@ model.connect('mux_all_sphere_radii.stacked_output', 'bbv.radii')
 # model.connect('collision_aggregation1.aggregated_output', 'collision_multiplexer.input_0')
 # model.connect('collision_aggregation2.aggregated_output', 'collision_multiplexer.input_1')
 # model.connect('collision_aggregation3.aggregated_output', 'collision_multiplexer.input_2')
-# # model.connect('collision_aggregation4.aggregated_output', 'collision_multiplexer.input_3')
+# model.connect('collision_aggregation4.aggregated_output', 'collision_multiplexer.input_3')
 
 
 # Define the variables, objective, and constraints
@@ -147,26 +169,33 @@ model.add_design_var('system.components.comp_2.translation', ref=10)
 model.add_design_var('system.components.comp_2.rotation', ref=2*3.14159)
 model.add_design_var('system.components.comp_3.translation', ref=10)
 model.add_design_var('system.components.comp_3.rotation', ref=2*3.14159)
+model.add_design_var('system.components.comp_4.translation', ref=10)
+model.add_design_var('system.components.comp_4.rotation', ref=2*3.14159)
+model.add_design_var('system.components.comp_5.translation', ref=10)
+model.add_design_var('system.components.comp_5.rotation', ref=2*3.14159)
+model.add_design_var('system.components.comp_6.translation', ref=10)
+model.add_design_var('system.components.comp_6.rotation', ref=2*3.14159)
+model.add_design_var('system.components.comp_7.translation', ref=10)
+model.add_design_var('system.components.comp_7.rotation', ref=2*3.14159)
+model.add_design_var('system.components.comp_8.translation', ref=10)
+model.add_design_var('system.components.comp_8.rotation', ref=2*3.14159)
+model.add_design_var('system.components.comp_9.translation', ref=10)
+model.add_design_var('system.components.comp_9.rotation', ref=2*3.14159)
 
 model.add_design_var('system.interconnects.int_0.control_points')
 model.add_design_var('system.interconnects.int_1.control_points')
-
-# # prob.model.add_design_var('components.pump_2a.translation', ref=10)
-# # prob.model.add_design_var('components.pump_2b.translation', ref=10)
-# # prob.model.add_design_var('components.particle_filter_3a.translation', ref=10)
-# # prob.model.add_design_var('components.particle_filter_3b.translation', ref=10)
-# # prob.model.add_design_var('components.fuel_cell_Stack_4a.translation', ref=10)
-# # prob.model.add_design_var('components.fuel_cell_Stack_4b.translation', ref=10)
-# # prob.model.add_design_var('components.WEG_heater_and_pump_5.translation', ref=10)
-# # prob.model.add_design_var('components.heater_core_6.translation', ref=10)
-
+model.add_design_var('system.interconnects.int_2.control_points')
+model.add_design_var('system.interconnects.int_3.control_points')
+model.add_design_var('system.interconnects.int_4.control_points')
+model.add_design_var('system.interconnects.int_5.control_points')
+model.add_design_var('system.interconnects.int_6.control_points')
+model.add_design_var('system.interconnects.int_7.control_points')
+model.add_design_var('system.interconnects.int_8.control_points')
+model.add_design_var('system.interconnects.int_9.control_points')
 
 prob.model.add_objective('bbv.bounding_box_volume', ref=1, ref0=0)
-# prob.model.add_constraint('collision_aggregation.aggregated_output', upper=0.0)
-# prob.model.add_constraint('collision_multiplexer.stacked_output', upper=0.0)
-# # # prob.model.add_constraint('g_c', upper=0)
-# # # prob.model.add_constraint('g_i', upper=0)
-# # # prob.model.add_constraint('g_ci', upper=0)
+prob.model.add_constraint('total_collision_aggregation.aggregated_output', upper=0.0)
+
 
 
 prob.driver = om.ScipyOptimizeDriver()
@@ -184,23 +213,23 @@ prob.set_val('system.components.comp_0.translation', [2, 7, 0])
 prob.set_val('system.components.comp_1.translation', [5.5, 7, 0])
 prob.set_val('system.components.comp_2.translation', [0.5, 6, 0])
 prob.set_val('system.components.comp_3.translation', [7.5, 6, 0])
-# # prob.set_val('components.comp_4.translation', [0.5, 4.5, 0])
-# # prob.set_val('components.comp_5.translation', [7.5, 5, 0])
-# # prob.set_val('components.comp_6.translation', [0.5, 2, 0])
-# # prob.set_val('components.comp_7.translation', [7.5, 3, 0])
-# # prob.set_val('components.comp_8.translation', [6, 1, 0])
-# # prob.set_val('components.comp_9.translation', [2, 0, 0])
+prob.set_val('system.components.comp_4.translation', [0.5, 4.5, 0])
+prob.set_val('system.components.comp_5.translation', [7.5, 5, 0])
+prob.set_val('system.components.comp_6.translation', [0.5, 2, 0])
+prob.set_val('system.components.comp_7.translation', [7.5, 3, 0])
+prob.set_val('system.components.comp_8.translation', [6, 1, 0])
+prob.set_val('system.components.comp_9.translation', [2, 0, 0])
 
 prob.set_val('system.interconnects.int_0.control_points', [[3.75, 7, 0]])
 prob.set_val('system.interconnects.int_1.control_points', [[7.50, 7, 0]])
-# # prob.set_val('interconnects.c2b_c3b.control_points', [[7.5, 5.47, 0]])
-# # prob.set_val('interconnects.c3b_c4b.control_points', [[7.5, 4.3175, 0]])
-# # prob.set_val('interconnects.c4b_c5.control_points', [[6.5, 2, 0]])
-# # prob.set_val('interconnects.c5_c6.control_points', [[3, 0, 0]])
-# # prob.set_val('interconnects.c6_c4a.control_points', [[0.5, 0, 0]])
-# # prob.set_val('interconnects.c4a_c3a.control_points', [[0.5, 3, 0]])
-# # prob.set_val('interconnects.c3a_c2a.control_points', [[0.5, 5, 0]])
-# # prob.set_val('interconnects.c2a_c1a.control_points', [[0.5, 6.5, 0]])
+prob.set_val('system.interconnects.int_2.control_points', [[7.5, 5.47, 0]])
+prob.set_val('system.interconnects.int_3.control_points', [[7.5, 4.3175, 0]])
+prob.set_val('system.interconnects.int_4.control_points', [[6.5, 2, 0]])
+prob.set_val('system.interconnects.int_5.control_points', [[3, 0, 0]])
+prob.set_val('system.interconnects.int_6.control_points', [[0.5, 0, 0]])
+prob.set_val('system.interconnects.int_7.control_points', [[0.5, 3, 0]])
+prob.set_val('system.interconnects.int_8.control_points', [[0.5, 5, 0]])
+prob.set_val('system.interconnects.int_9.control_points', [[0.5, 6.5, 0]])
 
 
 
--- src/SPI2py/API/constraints.py
+++ src/SPI2py/API/constraints.py
@@ -1,6 +1,7 @@
 import torch
 from torch.func import jacfwd
 from openmdao.api import ExplicitComponent, Group
+from itertools import combinations, product
 
 from SPI2py.models.kinematics.distance_calculations import signed_distances_spheres_spheres
 
@@ -91,104 +92,3 @@ class PairwiseCollisionDetection(ExplicitComponent):
     def _compute_signed_distances(positions_a, radii_a, positions_b, radii_b):
         signed_distances = signed_distances_spheres_spheres(positions_a, radii_a, positions_b, radii_b)
         return signed_distances
-
-
-# class _System:
-#     def __init__(self, components, interconnects):
-#
-#
-#         self.components = components
-#         self.interconnects = interconnects
-#         self.objects = self.components + self.interconnects
-#
-#
-#         self.component_pairs = self.get_component_pairs()
-#         self.interconnect_pairs = self.get_interconnect_pairs()
-#         self.component_interconnect_pairs = self.get_component_interconnect_pairs()
-#
-#
-#         objective = self.input['problem']['objective']
-#         self.set_objective(objective)
-#
-#         self.translations_shape = (self.num_components, 3)
-#         self.rotations_shape = (self.num_components, 3)
-#         self.routings_shape = (self.num_interconnects, self.num_nodes, 3)
-#
-#     # def set_objective(self, objective: str):
-#     #
-#     #     """
-#     #     Add an objective to the design study.
-#     #
-#     #     :param objective: The objective function to be added.
-#     #     :param options: The options for the objective function.
-#     #     """
-#     #
-#     #     # SELECT THE OBJECTIVE FUNCTION HANDLE
-#     #
-#     #     if objective == 'bounding box volume':
-#     #         _objective_function = bounding_box_volume
-#     #     else:
-#     #         raise NotImplementedError
-#     #
-#     #     def objective_function(positions):
-#     #         return _objective_function(positions)
-#     #
-#     #     self.objective = objective_function
-#     #
-#     # def calculate_positions(self, translations, rotations, routings):
-#     #
-#     #     objects_dict = {}
-#     #
-#     #     for component, translation, rotation in zip(self.components, translations, rotations):
-#     #         object_dict = component.calculate_positions(translation, rotation)
-#     #         objects_dict = {**objects_dict, **object_dict}
-#     #
-#     #     for interconnect, routing in zip(self.interconnects, routings):
-#     #         object_dict = interconnect.calculate_positions(routing)
-#     #         objects_dict = {**objects_dict, **object_dict}
-#     #
-#     #     return objects_dict
-#
-#     def get_component_pairs(self):
-#         component_component_pairs = list(combinations(self.components, 2))
-#         return component_component_pairs
-#
-#     def get_interconnect_pairs(self):
-#         interconnect_interconnect_pairs = list(combinations(self.interconnects, 2))
-#         return interconnect_interconnect_pairs
-#
-#     def get_component_interconnect_pairs(self):
-#         component_interconnect_pairs = list(product(self.components, self.interconnects))
-#         return component_interconnect_pairs
-#
-#     def collision_component_pairs(self, positions_dict):
-#         signed_distance_vals = aggregate_signed_distance(positions_dict, self.component_pairs)
-#         max_signed_distance = kreisselmeier_steinhauser(signed_distance_vals)
-#         return max_signed_distance
-#
-#     def collision_interconnect_pairs(self, positions_dict):
-#         signed_distance_vals = aggregate_signed_distance(positions_dict, self.interconnect_pairs)
-#         max_signed_distance = kreisselmeier_steinhauser(signed_distance_vals)
-#         return max_signed_distance
-#
-#     def collision_component_interconnect_pairs(self, positions_dict):
-#         # TODO Remove tolerance
-#         signed_distance_vals = aggregate_signed_distance(positions_dict, self.component_interconnect_pairs)
-#         max_signed_distance = kreisselmeier_steinhauser(signed_distance_vals) - 0.2
-#         return max_signed_distance
-#
-#
-#
-#     def calculate_constraints(self, translations, rotations, routings):
-#
-#         positions_dict = self.calculate_positions(translations, rotations, routings)
-#
-#         # g_component_pairs = self.collision_component_pairs(positions_dict)
-#         # g_interconnect_pairs = self.collision_interconnect_pairs(positions_dict)
-#         # g_component_interconnect_pairs = self.collision_component_interconnect_pairs(positions_dict)
-#         # g = torch.tensor((g_component_pairs, g_interconnect_pairs, g_component_interconnect_pairs))
-#
-#         # TODO Add other constraints back in
-#         g = self.collision_component_pairs(positions_dict)
-#
-#         return g
