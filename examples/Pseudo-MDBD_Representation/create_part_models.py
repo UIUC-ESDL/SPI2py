6fe2c1041178474513da94a2fe22e1c046300c5e
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2024-03-28 20:24:15 -0500

    Debugging MDBD Projection

--- examples/Pseudo-MDBD_Representation/create_part_models.py
+++ examples/Pseudo-MDBD_Representation/create_part_models.py
@@ -16,13 +16,13 @@ from SPI2py.models.geometry.finite_sphere_method import read_xyzr_file
 # mdbd_unit_cube = pv.Cube(bounds=(-0.5, 0.5, -0.5, 0.5, -0.5, 0.5))
 # mdbd_unit_cube.save('mdbd_unit_cube.stl')
 #
-mdbd('', 'mdbd_unit_cube.stl','mdbd_unit_cube.xyzr',
-     num_spheres=1000,
-     min_radius=0.0001,
-     meshgrid_increment=50,
-     plot=False)
+# mdbd('', 'mdbd_unit_cube.stl','mdbd_unit_cube.xyzr',
+#      num_spheres=3000,
+#      min_radius=0.0001,
+#      meshgrid_increment=125,
+#      plot=False)
 
-positions, radii = read_xyzr_file('mdbd_unit_cube.xyzr', num_spheres=1000)
+positions, radii = read_xyzr_file('mdbd_unit_cube.xyzr', num_spheres=3000)
 
 
 
@@ -31,24 +31,32 @@ positions, radii = read_xyzr_file('mdbd_unit_cube.xyzr', num_spheres=1000)
 # positions = np.array(positions)
 radii = np.array(radii)
 
+percent_accuracy = 0.933
 
 expected_max_volume = 1.0
 
 # Calculate cumulative volume
 cumulative_volume = []
 total_volume = 0
+target_volume = expected_max_volume * percent_accuracy
+num_spheres_required = 0
 for radius in radii:
     volume = 4.0/3.0*np.pi*radius**3
     total_volume += volume
     cumulative_volume.append(total_volume)
+    num_spheres_required += 1  # Increment counter
+    if total_volume >= target_volume:  # Check if cumulative volume has reached target volume
+        break  # If so, break the loop
 
-# Create scatter plot
-plt.scatter(range(len(radii)), cumulative_volume)
-plt.axhline(y=expected_max_volume, color='r', linestyle='--')  # Line for expected max volume
-plt.xlabel('Number of Spheres')
-plt.ylabel('Cumulative Volume')
-plt.title('Cumulative Volume vs Number of Spheres')
-plt.show()
+# # Create scatter plot
+# plt.scatter(range(len(radii)), cumulative_volume)
+# plt.axhline(y=expected_max_volume, color='r', linestyle='--')  # Line for expected max volume
+# plt.xlabel('Number of Spheres')
+# plt.ylabel('Cumulative Volume')
+# # Set y-axis from 0 to 1.3
+# plt.ylim(0, 1.3)
+# plt.title('Cumulative Volume vs Number of Spheres')
+# plt.show()
 
 
 
--- src/SPI2py/models/geometry/maximal_disjoint_ball_decomposition.py
+++ src/SPI2py/models/geometry/maximal_disjoint_ball_decomposition.py
@@ -8,120 +8,165 @@ import pyvista as pv
 import trimesh
 
 
+# def mdbd_2(directory, input_filename, output_filename, num_spheres=1000, min_radius=0.0001, meshgrid_increment=25, plot=True, color='green'):
+#
+#     # Create the pyvista and trimesh objects. Both are required.
+#     mesh_trimesh = trimesh.exchange.load.load(directory+input_filename)
+#
+#     # Define variable bounds based on the object's bounding box
+#     x_min = mesh_trimesh.vertices[:, 0].min()
+#     x_max = mesh_trimesh.vertices[:, 0].max()
+#     y_min = mesh_trimesh.vertices[:, 1].min()
+#     y_max = mesh_trimesh.vertices[:, 1].max()
+#     z_min = mesh_trimesh.vertices[:, 2].min()
+#     z_max = mesh_trimesh.vertices[:, 2].max()
+#
+#     # USER INPUT: The number of increments for each dimension of the meshgrid.
+#     nx = meshgrid_increment
+#     ny = meshgrid_increment
+#     nz = meshgrid_increment
+#
+#     # Create a 3d meshgrid
+#     x = np.linspace(x_min, x_max, nx)
+#     y = np.linspace(y_min, y_max, ny)
+#     z = np.linspace(z_min, z_max, nz)
+#     xx, yy, zz = np.meshgrid(x, y, z)
+#
+#     # Get all the points inside and outside the object
+#     all_points = np.vstack((xx.flatten(), yy.flatten(), zz.flatten())).T
+#
+#     # Get all the points inside the object
+#     signed_distances = trimesh.proximity.signed_distance(mesh_trimesh, all_points)
+#     interior_points = all_points[signed_distances > 0]
+#     interior_distances = signed_distances[signed_distances > 0]
+#
+#     # Sort the points by distance from the surface in descending order
+#     points_filtered_sorted = interior_points[np.argsort(interior_distances)[::-1]]
+#     distances_filtered_sorted = interior_distances[np.argsort(interior_distances)[::-1]]
+#
+#     # Initialize the empty arrays that will store the largest disjoint spheres in descending order
+#     sphere_points = np.empty((0, 3))
+#     sphere_radii = np.empty((0, 1))
+#
+#     maximin_distance = distances_filtered_sorted[0]
+#
+#     # Package spheres
+#     while maximin_distance > min_radius:
+#
+#         # Find the point furthest from the surface and existing spheres
+#         maximin_distance = distances_filtered_sorted[0]
+#         min_distance_point = points_filtered_sorted[0]
+#
+#         if maximin_distance < min_radius:
+#             "Breaking"
+#             break
+#
+#         # Remove the point from the list of points
+#         points_filtered_sorted = points_filtered_sorted[1:]
+#         distances_filtered_sorted = distances_filtered_sorted[1:]
+#
+#         # Add the point to the list of spheres
+#         sphere_points = np.vstack((sphere_points, min_distance_point))
+#         sphere_radii = np.vstack((sphere_radii, maximin_distance))
+#
+#         # Remove any points that are within the maximin sphere
+#         distances_filtered_sorted = distances_filtered_sorted[
+#             np.linalg.norm(points_filtered_sorted - min_distance_point, axis=1) > maximin_distance]
+#         points_filtered_sorted = points_filtered_sorted[np.linalg.norm(points_filtered_sorted - min_distance_point, axis=1) > maximin_distance]
+#
+#         if len(sphere_points) >= num_spheres:
+#             print('Max number of spheres reached')
+#             break
+#
+#     if plot:
+#         plotter = pv.Plotter()
+#
+#         spheres = []
+#         for i in range(len(sphere_points)):
+#             sphere = pv.Sphere(center=sphere_points[i], radius=sphere_radii[i])
+#             spheres.append(sphere)
+#
+#         merged = pv.MultiBlock(spheres).combine().extract_surface().clean()
+#         plotter.add_mesh(merged, color=color, opacity=0.95)
+#
+#         plotter.view_xz()
+#         plotter.background_color = 'white'
+#         plotter.show()
+#
+#     # OUTPUT
+#
+#     # Combine the points and radii into a single array
+#     spheres = np.hstack((sphere_points, sphere_radii))
+#
+#     # Write the spheres to a text file
+#     np.savetxt(directory+output_filename, spheres, delimiter=' ')
+
+
+
 def mdbd(directory, input_filename, output_filename, num_spheres=1000, min_radius=0.0001, meshgrid_increment=25, plot=True, color='green'):
 
-    # Create the pyvista and trimesh objects. Both are required.
-    mesh_trimesh = trimesh.exchange.load.load(directory+input_filename)
+    # Load the mesh using trimesh
+    mesh_trimesh = trimesh.load(directory + input_filename)
 
     # Define variable bounds based on the object's bounding box
-    x_min = mesh_trimesh.vertices[:, 0].min()
-    x_max = mesh_trimesh.vertices[:, 0].max()
-    y_min = mesh_trimesh.vertices[:, 1].min()
-    y_max = mesh_trimesh.vertices[:, 1].max()
-    z_min = mesh_trimesh.vertices[:, 2].min()
-    z_max = mesh_trimesh.vertices[:, 2].max()
+    bounds = mesh_trimesh.bounds
+    x_min, y_min, z_min = bounds[0]  # Min bounds
+    x_max, y_max, z_max = bounds[1]  # Max bounds
 
-    # USER INPUT: The number of increments for each dimension of the meshgrid.
-    nx = meshgrid_increment
-    ny = meshgrid_increment
-    nz = meshgrid_increment
+    # Create a 3D meshgrid within the bounds
+    x = np.linspace(x_min, x_max, meshgrid_increment)
+    y = np.linspace(y_min, y_max, meshgrid_increment)
+    z = np.linspace(z_min, z_max, meshgrid_increment)
+    xx, yy, zz = np.meshgrid(x, y, z, indexing='ij')
+    all_points = np.vstack([xx.ravel(), yy.ravel(), zz.ravel()]).T
 
-    # Create a 3d meshgrid
-    x = np.linspace(x_min, x_max, nx)
-    y = np.linspace(y_min, y_max, ny)
-    z = np.linspace(z_min, z_max, nz)
-    xx, yy, zz = np.meshgrid(x, y, z)
+    # Calculate signed distances for all points
+    signed_distances = trimesh.proximity.signed_distance(mesh_trimesh, all_points)
 
-    # All points inside and outside the object
-    points_a = np.vstack((xx.flatten(), yy.flatten(), zz.flatten())).T
+    # Filter points inside the mesh
+    interior_points = all_points[signed_distances > 0]
+    interior_distances = signed_distances[signed_distances > 0]
 
-    # All points inside the object
-    signed_distances = trimesh.proximity.signed_distance(mesh_trimesh, points_a)
-    points_filtered = points_a[signed_distances > 0]
-    distances_filtered = signed_distances[signed_distances > 0]
+    # Sort points by their distance from the surface, descending
+    sorted_indices = np.argsort(interior_distances)[::-1]
+    points_filtered_sorted = interior_points[sorted_indices]
+    distances_filtered_sorted = interior_distances[sorted_indices]
 
-    # Sort the points by distance from the surface in descending order
-    points_filtered_sorted = points_filtered[np.argsort(distances_filtered)[::-1]]
-    distances_filtered_sorted = distances_filtered[np.argsort(distances_filtered)[::-1]]
-
-    # Initialize the empty arrays that will store the MDBD spheres
     sphere_points = np.empty((0, 3))
     sphere_radii = np.empty((0, 1))
 
-    min_distance = distances_filtered_sorted[0]
-    # Package spheres
-    # for i in range(num_spheres):
-    while min_distance > min_radius:
+    # Iterate to pack spheres until reaching the limit or the smallest sphere is smaller than min_radius
+    while len(sphere_points) < num_spheres and distances_filtered_sorted.size > 0 and distances_filtered_sorted[0] > min_radius:
+        # Choose the point with the maximum distance from any surface or existing sphere
+        sphere_center = points_filtered_sorted[0]
+        sphere_radius = distances_filtered_sorted[0]
 
-        # Find the point furthest from the surface and existing spheres
-        min_distance = distances_filtered_sorted[0]
-        min_distance_point = points_filtered_sorted[0]
+        # Update lists of points and distances
+        sphere_points = np.vstack([sphere_points, sphere_center])
+        sphere_radii = np.vstack([sphere_radii, sphere_radius])
 
-        if min_distance < min_radius:
-            "Breaking"
-            break
-
-        # Remove the point from the list of points
-        points_filtered_sorted = points_filtered_sorted[1:]
-        distances_filtered_sorted = distances_filtered_sorted[1:]
-
-        sphere_points = np.vstack((sphere_points, min_distance_point))
-        sphere_radii = np.vstack((sphere_radii, min_distance))
-
-        # Remove any points that are within the max-min sphere
-        distances_filtered_sorted = distances_filtered_sorted[
-            np.linalg.norm(points_filtered_sorted - min_distance_point, axis=1) > min_distance]
-        points_filtered_sorted = points_filtered_sorted[np.linalg.norm(points_filtered_sorted - min_distance_point, axis=1) > min_distance]
-
-        if len(sphere_points) >= num_spheres:
-            print('Max number of spheres reached')
-            break
+        # Update distances considering the newly added sphere
+        point_distances_to_new_sphere = np.linalg.norm(points_filtered_sorted - sphere_center, axis=1)
+        r_tol = 0.1135  # Fudging factor
+        within_new_sphere = point_distances_to_new_sphere < sphere_radius + r_tol*sphere_radius
+        points_filtered_sorted = points_filtered_sorted[~within_new_sphere]
+        distances_filtered_sorted = distances_filtered_sorted[~within_new_sphere]
 
     if plot:
-        # Plot object with PyVista
         plotter = pv.Plotter()
 
-        part2 = pv.read(directory+input_filename)
-        part2_slice = part2.slice(normal=[0,1,0])
-        # bounds = [0, 1, 0, 1, 0, 1]
-        # clipped = dataset.clip_box(bounds)
-        # clipped = part2.clip_box(bounds)
-        clipped = part2.clip(normal='y')
-        # plotter.add_mesh(part2, color='gray', opacity=0.95, silhouette=True)
-        # plotter.add_mesh(clipped, color='gray', opacity=0.95, silhouette={'line_width': 2}, roughness=0)
-
-        # Plot the sphere
         spheres = []
         for i in range(len(sphere_points)):
             sphere = pv.Sphere(center=sphere_points[i], radius=sphere_radii[i])
-            # plotter.add_mesh(sphere, color='green', opacity=0.75)
-            # plotter.add_mesh(sphere, color='green', opacity=0.25)
             spheres.append(sphere)
 
         merged = pv.MultiBlock(spheres).combine().extract_surface().clean()
-        merged_clipped = merged.clip(normal='y')
-        # merged_sliced = merged.slice(normal=[0,1,0])
-        # plotter.add_mesh(merged_sliced, color='green', opacity=0.25)
-        # plotter.add_mesh(merged_sliced, color='green')
         plotter.add_mesh(merged, color=color, opacity=0.95)
-        # plotter.add_mesh(merged_clipped, color='green', opacity=0.25)
 
-        # plotter.view_isometric()
         plotter.view_xz()
-        # plotter.enable_shadows()
         plotter.background_color = 'white'
         plotter.show()
 
-
-
-
-        # slice = plotter.mesh.slice(normal=[1,1,0])
-        # slice.plot()
-
-    # OUTPUT
-
-    # Combine the points and radii into a single array
+    # OUTPUT: Save the spheres to a file
     spheres = np.hstack((sphere_points, sphere_radii))
-
-    # Write the spheres to a text file
-    np.savetxt(directory+output_filename, spheres, delimiter=' ')
+    np.savetxt(directory + output_filename, spheres, delimiter=' ')
