090459f1bc905ac848baf496dba16c251b3b9d15
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2024-03-31 20:14:08 -0500

    Re-implementing JAX

--- examples/Projection/single_projection.py
+++ examples/Projection/single_projection.py
@@ -124,7 +124,7 @@ print("Constraint Value: ", prob.get_val('volume_fraction_constraint.volume_frac
 t1 = time_ns()
 
 # Run the optimization
-# prob.run_driver()
+prob.run_driver()
 
 t2 = time_ns()
 print('Runtime: ', (t2 - t1) / 1e9, 's')
--- src/SPI2py/API/constraints.py
+++ src/SPI2py/API/constraints.py
@@ -1,103 +1,102 @@
-import torch
-from torch.func import jacfwd
+import jax.numpy as jnp
+from jax import jacfwd
 from openmdao.api import ExplicitComponent, Group
 
-from SPI2py.models.kinematics.distance_calculations import signed_distances_spheres_spheres
+from ..models.kinematics.distance_calculations import signed_distances_spheres_spheres
 
 
-class CombinatorialCollisionDetection(Group):
-    pass
-
-class PairwiseCollisionDetection(ExplicitComponent):
-
-    def initialize(self):
-        self.options.declare('n_spheres', types=int, desc='Number of spheres for input a')
-        self.options.declare('m_spheres', types=int, desc='Number of spheres for input b')
+# class CombinatorialCollisionDetection(Group):
+#     raise NotImplementedError
 
 
-    def setup(self):
-        n = self.options['n_spheres']
-        m = self.options['m_spheres']
-
-        self.add_input('positions_a', shape=(n, 3))
-        self.add_input('radii_a', shape=(n, 1))
-
-        self.add_input('positions_b', shape=(m, 3))
-        self.add_input('radii_b', shape=(m, 1))
-
-        self.add_output('signed_distances', shape=(n, m))
-
-    def setup_partials(self):
-        self.declare_partials('signed_distances', 'positions_a')
-        self.declare_partials('signed_distances', 'radii_a')
-        self.declare_partials('signed_distances', 'positions_b')
-        self.declare_partials('signed_distances', 'radii_b')
-
-
-    def compute(self, inputs, outputs):
-
-        # Extract the inputs
-        positions_a = inputs['positions_a']
-        radii_a = inputs['radii_a']
-        positions_b = inputs['positions_b']
-        radii_b = inputs['radii_b']
-
-        # Convert the inputs to torch tensors
-        positions_a = torch.tensor(positions_a, dtype=torch.float64)
-        radii_a = torch.tensor(radii_a, dtype=torch.float64)
-        positions_b = torch.tensor(positions_b, dtype=torch.float64)
-        radii_b = torch.tensor(radii_b, dtype=torch.float64)
-
-        # Calculate the signed distances
-        signed_distances = self._compute_signed_distances(positions_a, radii_a, positions_b, radii_b)
-
-        # Convert the outputs to numpy arrays
-        signed_distances = signed_distances.detach().numpy()
-
-        # Write the outputs
-        outputs['signed_distances'] = signed_distances
-
-    def compute_partials(self, inputs, partials):
-
-        # Extract the inputs
-        positions_a = inputs['positions_a']
-        radii_a = inputs['radii_a']
-        positions_b = inputs['positions_b']
-        radii_b = inputs['radii_b']
-
-        # Convert the inputs to torch tensors
-        positions_a = torch.tensor(positions_a, dtype=torch.float64, requires_grad=True)
-        radii_a = torch.tensor(radii_a, dtype=torch.float64, requires_grad=True)
-        positions_b = torch.tensor(positions_b, dtype=torch.float64, requires_grad=True)
-        radii_b = torch.tensor(radii_b, dtype=torch.float64, requires_grad=True)
-
-        # Define the Jacobian matrices using PyTorch Autograd
-        jac_signed_distances = jacfwd(self._compute_signed_distances, argnums=(0, 1, 2, 3))
-
-        # Evaluate the Jacobian matrices
-        jac_signed_distances_vals = jac_signed_distances(positions_a, radii_a, positions_b, radii_b)
-
-        # Slice the Jacobian
-        jac_signed_distances_positions_a = jac_signed_distances_vals[0].detach().numpy()
-        jac_signed_distances_radii_a = jac_signed_distances_vals[1].detach().numpy()
-        jac_signed_distances_positions_b = jac_signed_distances_vals[2].detach().numpy()
-        jac_signed_distances_radii_b = jac_signed_distances_vals[3].detach().numpy()
-
-        # Write the outputs
-        partials['signed_distances', 'positions_a'] = jac_signed_distances_positions_a
-        partials['signed_distances', 'radii_a'] = jac_signed_distances_radii_a
-        partials['signed_distances', 'positions_b'] = jac_signed_distances_positions_b
-        partials['signed_distances', 'radii_b'] = jac_signed_distances_radii_b
-
-    @staticmethod
-    def _compute_signed_distances(positions_a, radii_a, positions_b, radii_b):
-        signed_distances = signed_distances_spheres_spheres(positions_a, radii_a, positions_b, radii_b)
-        return signed_distances
+# class PairwiseCollisionDetection(ExplicitComponent):
+#
+#     def initialize(self):
+#         self.options.declare('n_spheres', types=int, desc='Number of spheres for input a')
+#         self.options.declare('m_spheres', types=int, desc='Number of spheres for input b')
+#
+#     def setup(self):
+#         n = self.options['n_spheres']
+#         m = self.options['m_spheres']
+#
+#         self.add_input('positions_a', shape=(n, 3))
+#         self.add_input('radii_a', shape=(n, 1))
+#
+#         self.add_input('positions_b', shape=(m, 3))
+#         self.add_input('radii_b', shape=(m, 1))
+#
+#         self.add_output('signed_distances', shape=(n, m))
+#
+#     def setup_partials(self):
+#         self.declare_partials('signed_distances', 'positions_a')
+#         self.declare_partials('signed_distances', 'radii_a')
+#         self.declare_partials('signed_distances', 'positions_b')
+#         self.declare_partials('signed_distances', 'radii_b')
+#
+#     def compute(self, inputs, outputs):
+#
+#         # Extract the inputs
+#         positions_a = inputs['positions_a']
+#         radii_a = inputs['radii_a']
+#         positions_b = inputs['positions_b']
+#         radii_b = inputs['radii_b']
+#
+#         # Convert the inputs to torch tensors
+#         positions_a = torch.tensor(positions_a, dtype=torch.float64)
+#         radii_a = torch.tensor(radii_a, dtype=torch.float64)
+#         positions_b = torch.tensor(positions_b, dtype=torch.float64)
+#         radii_b = torch.tensor(radii_b, dtype=torch.float64)
+#
+#         # Calculate the signed distances
+#         signed_distances = self._compute_signed_distances(positions_a, radii_a, positions_b, radii_b)
+#
+#         # Convert the outputs to numpy arrays
+#         signed_distances = signed_distances.detach().numpy()
+#
+#         # Write the outputs
+#         outputs['signed_distances'] = signed_distances
+#
+#     def compute_partials(self, inputs, partials):
+#
+#         # Extract the inputs
+#         positions_a = inputs['positions_a']
+#         radii_a = inputs['radii_a']
+#         positions_b = inputs['positions_b']
+#         radii_b = inputs['radii_b']
+#
+#         # Convert the inputs to torch tensors
+#         positions_a = torch.tensor(positions_a, dtype=torch.float64, requires_grad=True)
+#         radii_a = torch.tensor(radii_a, dtype=torch.float64, requires_grad=True)
+#         positions_b = torch.tensor(positions_b, dtype=torch.float64, requires_grad=True)
+#         radii_b = torch.tensor(radii_b, dtype=torch.float64, requires_grad=True)
+#
+#         # Define the Jacobian matrices using PyTorch Autograd
+#         jac_signed_distances = jacfwd(self._compute_signed_distances, argnums=(0, 1, 2, 3))
+#
+#         # Evaluate the Jacobian matrices
+#         jac_signed_distances_vals = jac_signed_distances(positions_a, radii_a, positions_b, radii_b)
+#
+#         # Slice the Jacobian
+#         jac_signed_distances_positions_a = jac_signed_distances_vals[0].detach().numpy()
+#         jac_signed_distances_radii_a = jac_signed_distances_vals[1].detach().numpy()
+#         jac_signed_distances_positions_b = jac_signed_distances_vals[2].detach().numpy()
+#         jac_signed_distances_radii_b = jac_signed_distances_vals[3].detach().numpy()
+#
+#         # Write the outputs
+#         partials['signed_distances', 'positions_a'] = jac_signed_distances_positions_a
+#         partials['signed_distances', 'radii_a'] = jac_signed_distances_radii_a
+#         partials['signed_distances', 'positions_b'] = jac_signed_distances_positions_b
+#         partials['signed_distances', 'radii_b'] = jac_signed_distances_radii_b
+#
+#     @staticmethod
+#     def _compute_signed_distances(positions_a, radii_a, positions_b, radii_b):
+#         signed_distances = signed_distances_spheres_spheres(positions_a, radii_a, positions_b, radii_b)
+#         return signed_distances
 
 
 class VolumeFractionConstraint(ExplicitComponent):
     def initialize(self):
-        self.options.declare('n_projections', types=int, desc='Number of projections')
+        self.options.declare('n_projections', types=int, desc='Number of objects with projections')
         self.options.declare('relative_volume_tolerance', types=float, desc='Relative volume tolerance')
 
     def setup(self):
@@ -122,7 +121,7 @@ class VolumeFractionConstraint(ExplicitComponent):
 
         # Calculate the volume
         element_length = inputs['element_length']
-        element_length = torch.tensor(element_length, dtype=torch.float64)
+        element_length = jnp.array(element_length)
 
         # Extract the inputs and convert to a numpy array
         element_pseudo_densities = []
@@ -131,7 +130,7 @@ class VolumeFractionConstraint(ExplicitComponent):
 
         element_pseudo_densities_tensors = ()
         for i in range(n):
-            element_pseudo_densities_tensors = element_pseudo_densities_tensors + (torch.tensor(element_pseudo_densities[i], dtype=torch.float64),)
+            element_pseudo_densities_tensors = element_pseudo_densities_tensors + (jnp.array(element_pseudo_densities[i]),)
 
         volume_fraction_constraint = self._volume_fraction_constraint(element_length, *element_pseudo_densities_tensors)
 
@@ -145,7 +144,8 @@ class VolumeFractionConstraint(ExplicitComponent):
 
         # Calculate the volume
         element_length = inputs['element_length']
-        element_length = torch.tensor(element_length, dtype=torch.float64, requires_grad=True)
+        # element_length = torch.tensor(element_length, dtype=torch.float64, requires_grad=True)
+        element_length = jnp.array(element_length)
 
         # Extract the inputs and convert to a numpy array
         element_pseudo_densities = ()
@@ -154,7 +154,8 @@ class VolumeFractionConstraint(ExplicitComponent):
 
         element_pseudo_densities_tensors = ()
         for i in range(n):
-            element_pseudo_densities_tensors = element_pseudo_densities_tensors + (torch.tensor(element_pseudo_densities[i], dtype=torch.float64, requires_grad=True),)
+            # element_pseudo_densities_tensors = element_pseudo_densities_tensors + (torch.tensor(element_pseudo_densities[i], dtype=torch.float64, requires_grad=True),)
+            element_pseudo_densities_tensors = element_pseudo_densities_tensors + (jnp.array(element_pseudo_densities[i]),)
 
         # Calculate the partial derivatives wrt all inputs
         argnums = tuple(range(n))
@@ -163,7 +164,7 @@ class VolumeFractionConstraint(ExplicitComponent):
         # Convert the partial derivatives to numpy arrays
         jac_volume_fraction_constraint_np = []
         for jac in jac_volume_fraction_constraint:
-            jac_volume_fraction_constraint_np.append(jac.detach().numpy())
+            jac_volume_fraction_constraint_np.append(jac)
 
         # Set the partial derivatives
         for i in range(n):
@@ -183,7 +184,7 @@ class VolumeFractionConstraint(ExplicitComponent):
         # Calculate the sum of the combined volumes
 
         # Add all the element pseudo-densities together
-        combined_pseudo_densities = torch.zeros_like(element_pseudo_densities[0])
+        combined_pseudo_densities = jnp.zeros_like(element_pseudo_densities[0])
         for i in range(len(element_pseudo_densities)):
             combined_pseudo_densities += element_pseudo_densities[i]
 
