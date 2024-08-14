117093e3591605c37d9d80de4296db3435a489a9
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2024-08-13 19:14:55 -0500

    adding unit tests

--- examples/scratch/project_cylinder.py
+++ examples/scratch/project_cylinder.py
@@ -117,6 +117,51 @@ def rho_b(phi_b, r):
         raise ValueError('Something went wrong')
 
 
+def calculate_density(position, radius, x1, x2, r):
+    phi = phi_b(position, x1, x2, r)
+    rho = rho_b(phi, radius)
+    return rho
+
+def calculate_combined_density(position, radius, X1, X2, R):
+
+    combined_density = 0
+    for x1, x2, r in zip(X1, X2, R):
+        density = calculate_density(position, radius, x1, x2, r)
+        combined_density += density
+
+    # Clip the combined densities to be between 0 and 1
+    combined_density = max(0, min(combined_density, 1))
+
+    return combined_density
+
+
+# Create grid
+n, m, o = 5, 5, 2
+positions, radii = create_grid(n, m, o, spacing=1)
+
+# Create line segment arrays
+line_segment_points = [(0, 0, 0), (2, 2, 0), (2, 4, 0)]
+line_segment_radius = 0.25
+X1, X2, R = create_cylinders(line_segment_points, line_segment_radius)
+
+# Calculate the projected densities
+densities = np.zeros((n, m, o))
+for i in range(n):
+    for j in range(m):
+        for k in range(o):
+            position = positions[i, j, k]
+            radius = radii[i, j, k]
+            combined_density = calculate_combined_density(position, radius, X1, X2, R)
+            densities[i, j, k] += combined_density
+
+plot_density_grid(positions, radii, X1.reshape(-1, 3), X2.reshape(-1, 3), R.reshape(-1, 1), densities)
+
+
+
+
+
+
+
 # def calculate_densities(positions, radii, x1, x2, r):
 #
 #     densities = np.zeros_like(radii)
@@ -141,161 +186,6 @@ def rho_b(phi_b, r):
 #     combined_densities = np.clip(combined_densities, 0, 1)
 #
 #     return combined_densities
-
-
-def calculate_density(position, radius, x1, x2, r):
-    phi = phi_b(position, x1, x2, r)
-    rho = rho_b(phi, radius)
-    return rho
-
-def calculate_combined_density(position, radius, X1, X2, R):
-
-    combined_density = 0
-    for x1, x2, r in zip(X1, X2, R):
-        density = calculate_density(position, radius, x1, x2, r)
-        combined_density += density
-
-    # Clip the combined densities to be between 0 and 1
-    combined_density = max(0, min(combined_density, 1))
-
-    return combined_density
-
-
-# Example usage
-n, m, o = 10, 10, 10
-line_segment_points = [(0, 0, 0), (2.5, 2.5, 0), (2.5, 5, 0)]
-line_segment_radius = 0.5
-
-
-# Create grid
-positions, radii = create_grid(n, m, o, spacing=0.5)
-
-# Create line segment arrays
-X1, X2, R = create_cylinders(line_segment_points, line_segment_radius)
-
-
-densities = np.zeros((n, m, o))
-for i in range(n):
-    for j in range(m):
-        for k in range(o):
-            position = positions[i, j, k]
-            radius = radii[i, j, k]
-            combined_density = calculate_combined_density(position, radius, X1, X2, R)
-            densities[i, j, k] += combined_density
-
-# print(densities)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# phi = phi_b(np.array([[4, 4, 0]]), x1, x2, r)
-# rho = rho_b(phi, 0.5)
-#
-# print('Density', rho)
-
-# Calculate densities
-# densities_array = calculate_densities(positions_flat, radii_flat, x1, x2, r)
-
-plot_density_grid(positions, radii, X1.reshape(-1, 3), X2.reshape(-1, 3), R.reshape(-1, 1), densities)
-
-
-
-
-# sphere_positions, sphere_radii,
-#                       cylinder_start_positions, cylinder_stop_positions, cylinder_radii,
-#                       densities
-
-
-# plot_density_grid(positions, radii, combined_density, line_segment_points)
-#
-
-
-
-# # def penalized_density(rho, alpha, q):
-# #     return (alpha * rho) ** q
-#
-# # Define the 3D gri
-#
-# grid_spheres = create_grid_spheres(n, m, o, grid_spacing)
-#
-# # Define the pipe as a series of linear spline segments
-# pipe_radius = 0.5
-# pipe_segments = [
-#     (np.array([2, 2, 2]), np.array([4, 4, 2])),
-#     (np.array([4, 4, 2]), np.array([6, 4, 4])),
-#     (np.array([6, 4, 4]), np.array([8, 6, 6]))
-# ]
-#
-# pipe_cylinders = create_pipe(pipe_segments, pipe_radius)
-#
-# # Visualize using PyVista
-# plotter = pv.Plotter(shape=(1, 3))
-#
-# # Plot the grid spheres
-# plotter.subplot(0, 0)
-# for sphere in grid_spheres:
-#     plotter.add_mesh(sphere, color='white', opacity=0.5)
-# plotter.add_text("Grid Spheres", position='upper_edge')
-#
-# # Plot the pipe
-# plotter.subplot(0, 1)
-# for cylinder in pipe_cylinders:
-#     plotter.add_mesh(cylinder, color='blue', opacity=0.7)
-# plotter.add_text("Pipe Segments", position='upper_edge')
-#
-# # Calculate and plot the projected densities
-# plotter.subplot(0, 2)
-#
-# # Assuming rho_penalized is calculated for each grid cell (you would need to apply your functions)
-# # Here, we'll use random values as a placeholder
-# density_values = np.random.rand(n, m, o)
-#
-# # Example data
-# x = np.array([[1, 2, 3], [4, 5, 6]])
-# x1 = np.array([[0, 0, 0], [1, 1, 1]])
-# x2 = np.array([[3, 3, 3], [7, 7, 7]])
-# r_b = np.array([[1], [2]])
-#
-# # Calculate signed distances
-# distances = signed_distance(x, x1, x2, r_b)
-# print("Distances (vectorized): \n", distances)
-#
-# distances0 = signed_distance_s(x[0], x1[0], x2[0], r_b[0])
-# distances1 = signed_distance_s(x[1], x1[1], x2[1], r_b[1])
-# print("Distances: \n", distances0, '\n', distances1)
-#
-# # phi_v = signed_distance(x, x1, x2, r_b)
-#
-# # phi = signed_distance(x, x1, x2, r_b)
-# # rho = projected_density(phi, r)
-# # rho_penalized = penalized_density(rho, alpha, q)
-#
-# # for i in range(n):
-# #     for j in range(m):
-# #         for k in range(o):
-# #             center = np.array([i, j, k]) * grid_spacing
-# #             sphere = pv.Sphere(radius=grid_spacing / 2, center=center)
-# #             color_value = density_values[i, j, k]
-# #             color = cm.coolwarm(color_value)[:3]  # Convert to RGB
-# #             plotter.add_mesh(sphere, color=color, opacity=0.5)
-# #
-# # plotter.add_text("Projected Densities", position='upper_edge')
-# #
-# # # Link all three views together
-# # plotter.link_views()
-# #
-# # # Show the plots
-# # plotter.show()
 
 
 
--- tests/test_models/test_kinematics/test_distance_calculations/test_minimum_distance_segment_segment.py
+++ tests/test_models/test_kinematics/test_distance_calculations/test_minimum_distance_segment_segment.py
@@ -1,224 +1,446 @@
-# import numpy as np
-# from SPI2py.models.group_spatial.component_kinematic.distance_calculations import minimum_distance_segment_segment
-#
-# # TODO Add tests for all negative coordinates and mixed coordinates
-#
-# # AB is a Point and CD is a Point
-#
-#
-# def test_point_point_overlap():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([0., 0., 0.])
-#     c = np.array([0., 0., 0.])
-#     d = np.array([0., 0., 0.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 0.0)
-#
-#
-# def test_point_point_overlap_positive_coords():
-#
-#     a = np.array([1., 1., 1.])
-#     b = np.array([1., 1., 1.])
-#     c = np.array([1., 1., 1.])
-#     d = np.array([1., 1., 1.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 0.0)
-#
-#
-# def test_point_point_overlap_negative_coords():
-#
-#     a = np.array([-1., -1., -1.])
-#     b = np.array([-1., -1., -1.])
-#     c = np.array([-1., -1., -1.])
-#     d = np.array([-1., -1., -1.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 0.0)
-#
-#
-# def test_point_point_overlap_mixed_coords():
-#
-#     a = np.array([-1., 1., 0.])
-#     b = np.array([-1., 1., 0.])
-#     c = np.array([-1., 1., 0.])
-#     d = np.array([-1., 1., 0.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 0.0)
-#
-#
-#
-# def test_point_point_non_overlap():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([0., 0., 0.])
-#     c = np.array([1., 0., 0.])
-#     d = np.array([1., 0., 0.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 1.0)
-#
-#
-# # AB is a Point and CD is a Line Segment
-#
-#
-# def test_point_linesegment_overlap_1():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([0., 0., 0.])
-#     c = np.array([0., 0., 0.])
-#     d = np.array([1., 0., 0.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 0.0)
-#
-#
-# def test_point_linesegment_non_overlap_1():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([0., 0., 0.])
-#     c = np.array([0., 0., 1.])
-#     d = np.array([1., 0., 1.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 1.0)
-#
-#
-# # AB is a Line Segment and CD is a Point
-#
-# def test_linesegment_point_overlap_2():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([1., 0., 0.])
-#     c = np.array([0., 0., 0.])
-#     d = np.array([0., 0., 0.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 0.0)
-#
-#
-# def test_linesegment_point_non_overlap_2():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([1., 0., 0.])
-#     c = np.array([1., 0., 1.])
-#     d = np.array([1., 0., 1.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 1.0)
-#
-#
-# # AB is a Line Segment and CD is a Line Segment
-#
-#
-# def test_fully_overlapping():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([1., 0., 0.])
-#     c = np.array([0., 0., 0.])
-#     d = np.array([1., 0., 0.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 0.0)
-#
-#
-# def test_partially_overlapping():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([1., 0., 0.])
-#     c = np.array([0.5, 0., 0.])
-#     d = np.array([1.5, 0., 0.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 0.0)
-#
-#
-# def test_parallel_horizontal_within_range():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([1., 0., 0.])
-#     c = np.array([0., 0., 1.])
-#     d = np.array([1., 0., 1.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 1.0)
-#
-#
-# def test_parallel_horizontal_out_of_range():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([1., 0., 0.])
-#     c = np.array([2., 0., 1.])
-#     d = np.array([3., 0., 1.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     expected_dist = np.linalg.norm(c-b)
-#
-#     assert np.isclose(dist, expected_dist)
-#
-#
-# def test_parallel_horizontal_along_same_axis():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([1., 0., 0.])
-#     c = np.array([2., 0., 0.])
-#     d = np.array([3., 0., 0.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 1.0)
-#
-#
-# def test_parallel_vertical_within_range():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([0., 1., 0.])
-#     c = np.array([0., 0., 1.])
-#     d = np.array([0., 1., 1.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 1.0)
-#
-#
-# def test_parallel_vertical_out_of_range():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([0., 1., 0.])
-#     c = np.array([1., 2., 0.])
-#     d = np.array([1., 3., 0.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     expected_dist = np.linalg.norm(c - b)
-#
-#     assert np.isclose(dist, expected_dist)
-#
-#
-# def test_skew():
-#
-#     a = np.array([0., 0., 0.])
-#     b = np.array([1., 0., 0.])
-#     c = np.array([0., 0., 2.])
-#     d = np.array([1., 0., 1.])
-#
-#     dist, _ = minimum_distance_segment_segment(a, b, c, d)
-#
-#     assert np.isclose(dist, 1.0)
-#
+import numpy as np
+from SPI2py.models.kinematics.distance_calculations import minimum_distance_segment_segment
+
+# AB is a Point and CD is a Point
+
+
+def test_point_point_overlap():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([0., 0., 0.])
+    c = np.array([0., 0., 0.])
+    d = np.array([0., 0., 0.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 0.0)
+
+
+def test_point_point_overlap_positive_coords():
+
+    a = np.array([1., 1., 1.])
+    b = np.array([1., 1., 1.])
+    c = np.array([1., 1., 1.])
+    d = np.array([1., 1., 1.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 0.0)
+
+
+def test_point_point_overlap_negative_coords():
+
+    a = np.array([-1., -1., -1.])
+    b = np.array([-1., -1., -1.])
+    c = np.array([-1., -1., -1.])
+    d = np.array([-1., -1., -1.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 0.0)
+
+
+def test_point_point_overlap_mixed_coords():
+
+    a = np.array([-1., 1., 0.])
+    b = np.array([-1., 1., 0.])
+    c = np.array([-1., 1., 0.])
+    d = np.array([-1., 1., 0.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 0.0)
+
+
+
+def test_point_point_non_overlap():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([0., 0., 0.])
+    c = np.array([1., 0., 0.])
+    d = np.array([1., 0., 0.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 1.0)
+
+
+# AB is a Point and CD is a Line Segment
+
+
+def test_point_linesegment_overlap_1():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([0., 0., 0.])
+    c = np.array([0., 0., 0.])
+    d = np.array([1., 0., 0.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 0.0)
+
+
+def test_point_linesegment_non_overlap_1():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([0., 0., 0.])
+    c = np.array([0., 0., 1.])
+    d = np.array([1., 0., 1.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 1.0)
+
+
+# AB is a Line Segment and CD is a Point
+
+def test_linesegment_point_overlap_2():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([1., 0., 0.])
+    c = np.array([0., 0., 0.])
+    d = np.array([0., 0., 0.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 0.0)
+
+
+def test_linesegment_point_non_overlap_2():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([1., 0., 0.])
+    c = np.array([1., 0., 1.])
+    d = np.array([1., 0., 1.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 1.0)
+
+
+# AB is a Line Segment and CD is a Line Segment
+
+
+def test_fully_overlapping():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([1., 0., 0.])
+    c = np.array([0., 0., 0.])
+    d = np.array([1., 0., 0.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 0.0)
+
+
+def test_partially_overlapping():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([1., 0., 0.])
+    c = np.array([0.5, 0., 0.])
+    d = np.array([1.5, 0., 0.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 0.0)
+
+
+def test_parallel_horizontal_within_range():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([1., 0., 0.])
+    c = np.array([0., 0., 1.])
+    d = np.array([1., 0., 1.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 1.0)
+
+
+def test_parallel_horizontal_out_of_range():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([1., 0., 0.])
+    c = np.array([2., 0., 1.])
+    d = np.array([3., 0., 1.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    expected_dist = np.linalg.norm(c-b)
+
+    assert np.isclose(dist, expected_dist)
+
+
+def test_parallel_horizontal_along_same_axis():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([1., 0., 0.])
+    c = np.array([2., 0., 0.])
+    d = np.array([3., 0., 0.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 1.0)
+
+
+def test_parallel_vertical_within_range():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([0., 1., 0.])
+    c = np.array([0., 0., 1.])
+    d = np.array([0., 1., 1.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 1.0)
+
+
+def test_parallel_vertical_out_of_range():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([0., 1., 0.])
+    c = np.array([1., 2., 0.])
+    d = np.array([1., 3., 0.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    expected_dist = np.linalg.norm(c - b)
+
+    assert np.isclose(dist, expected_dist)
+
+
+def test_skew():
+
+    a = np.array([0., 0., 0.])
+    b = np.array([1., 0., 0.])
+    c = np.array([0., 0., 2.])
+    d = np.array([1., 0., 1.])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.isclose(dist, 1.0)
+
+
+# VECTORIZED
+
+
+# AB is a Point and CD is a Point
+
+
+def test_point_point_overlap_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[0., 0., 0.], [0., 0., 0.]])
+    c = np.array([[0., 0., 0.], [0., 0., 0.]])
+    d = np.array([[0., 0., 0.], [0., 0., 0.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 0.0))
+
+
+def test_point_point_overlap_positive_coords_vectorized():
+
+    a = np.array([[1., 1., 1.], [1., 1., 1.]])
+    b = np.array([[1., 1., 1.], [1., 1., 1.]])
+    c = np.array([[1., 1., 1.], [1., 1., 1.]])
+    d = np.array([[1., 1., 1.], [1., 1., 1.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 0.0))
+
+
+def test_point_point_overlap_negative_coords_vectorized():
+
+    a = np.array([[-1., -1., -1.], [-1., -1., -1.]])
+    b = np.array([[-1., -1., -1.], [-1., -1., -1.]])
+    c = np.array([[-1., -1., -1.], [-1., -1., -1.]])
+    d = np.array([[-1., -1., -1.], [-1., -1., -1.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 0.0))
+
+
+def test_point_point_overlap_mixed_coords_vectorized():
+
+    a = np.array([[-1., 1., 0.], [-1., 1., 0.]])
+    b = np.array([[-1., 1., 0.], [-1., 1., 0.]])
+    c = np.array([[-1., 1., 0.], [-1., 1., 0.]])
+    d = np.array([[-1., 1., 0.], [-1., 1., 0.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 0.0))
+
+
+def test_point_point_non_overlap_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[0., 0., 0.], [0., 0., 0.]])
+    c = np.array([[1., 0., 0.], [1., 0., 0.]])
+    d = np.array([[1., 0., 0.], [1., 0., 0.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 1.0))
+
+
+# AB is a Point and CD is a Line Segment
+
+
+def test_point_linesegment_overlap_1_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[0., 0., 0.], [0., 0., 0.]])
+    c = np.array([[0., 0., 0.], [0., 0., 0.]])
+    d = np.array([[1., 0., 0.], [1., 0., 0.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 0.0))
+
+
+def test_point_linesegment_non_overlap_1_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[0., 0., 0.], [0., 0., 0.]])
+    c = np.array([[0., 0., 1.], [0., 0., 1.]])
+    d = np.array([[1., 0., 1.], [1., 0., 1.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 1.0))
+
+
+# AB is a Line Segment and CD is a Point
+
+# FIXME
+def test_linesegment_point_overlap_2_vectorized():
+
+    a = np.array([[0., 0., 0.],[0., 0., 0.]])
+    b = np.array([[1., 0., 0.],[1., 0., 0.]])
+    c = np.array([[0., 0., 0.],[0., 0., 0.]])
+    d = np.array([[0., 0., 0.],[0., 0., 0.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 0.0))
+
+
+def test_linesegment_point_non_overlap_2_vectorized():
+
+    a = np.array([[0., 0., 0.],[0., 0., 0.]])
+    b = np.array([[1., 0., 0.],[1., 0., 0.]])
+    c = np.array([[1., 0., 1.],[1., 0., 1.]])
+    d = np.array([[1., 0., 1.],[1., 0., 1.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 1.0))
+
+
+# AB is a Line Segment and CD is a Line Segment
+
+
+def test_fully_overlapping_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[1., 0., 0.], [1., 0., 0.]])
+    c = np.array([[0., 0., 0.], [0., 0., 0.]])
+    d = np.array([[1., 0., 0.], [1., 0., 0.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 0.0))
+
+
+def test_partially_overlapping_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[1., 0., 0.], [1., 0., 0.]])
+    c = np.array([[0.5, 0., 0.], [0.5, 0., 0.]])
+    d = np.array([[1.5, 0., 0.], [1.5, 0., 0.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 0.0))
+
+
+def test_parallel_horizontal_within_range_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[1., 0., 0.], [1., 0., 0.]])
+    c = np.array([[0., 0., 1.], [0., 0., 1.]])
+    d = np.array([[1., 0., 1.], [1., 0., 1.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 1.0))
+
+
+def test_parallel_horizontal_out_of_range_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[1., 0., 0.], [1., 0., 0.]])
+    c = np.array([[2., 0., 1.], [2., 0., 1.]])
+    d = np.array([[3., 0., 1.], [3., 0., 1.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    expected_dist = np.linalg.norm(c-b, axis=1)
+
+    assert np.all(np.isclose(dist, expected_dist))
+
+
+def test_parallel_horizontal_along_same_axis_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[1., 0., 0.], [1., 0., 0.]])
+    c = np.array([[2., 0., 0.], [2., 0., 0.]])
+    d = np.array([[3., 0., 0.], [3., 0., 0.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 1.0))
+
+
+def test_parallel_vertical_within_range_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[0., 1., 0.], [0., 1., 0.]])
+    c = np.array([[0., 0., 1.], [0., 0., 1.]])
+    d = np.array([[0., 1., 1.], [0., 1., 1.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 1.0))
+
+
+
+def test_parallel_vertical_out_of_range_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[0., 1., 0.], [0., 1., 0.]])
+    c = np.array([[1., 2., 0.], [1., 2., 0.]])
+    d = np.array([[1., 3., 0.], [1., 3., 0.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    expected_dist = np.linalg.norm(c - b, axis=1)
+
+    assert np.all(np.isclose(dist, expected_dist))
+
+
+def test_skew_vectorized():
+
+    a = np.array([[0., 0., 0.], [0., 0., 0.]])
+    b = np.array([[1., 0., 0.], [1., 0., 0.]])
+    c = np.array([[0., 0., 2.], [0., 0., 2.]])
+    d = np.array([[1., 0., 1.], [1., 0., 1.]])
+
+    dist = minimum_distance_segment_segment(a, b, c, d)
+
+    assert np.all(np.isclose(dist, 1.0))
+
